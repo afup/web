a91e55dda7ea74ba9eab1ba66fd4b9f299197f40
Olivier Hoareau <olivier@phppro.fr>
2010-03-01 16:14:54 +0000

    * bug fix sur include path

--- sources/Afup/site/importer_spip.php
+++ sources/Afup/site/importer_spip.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once dirname(__FILE__) . '/../../../../sources/Afup/Bootstrap/Cli.php';
+require_once dirname(__FILE__) . '/../Bootstrap/Cli.php';
 
-require_once 'afup/AFUP_Site.php';
+require_once 'Afup/AFUP_Site.php';
 
 $site = new AFUP_Site();
 $site->importer_spip();
