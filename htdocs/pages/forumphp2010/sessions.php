d9ac51d96878bb3a9c17cd5518540e3c61dff847
Perrick Penet-Avez <perrick@noparking.net>
2010-08-30 20:08:22 +0000

    Remise en place des pages pour agenda, conférenciers et sessions

--- htdocs/pages/forumphp2010/sessions.php
+++ htdocs/pages/forumphp2010/sessions.php
@@ -1,9 +1,10 @@
 <?php
 require_once '../../include/prepend.inc.php';
 require_once dirname(__FILE__) . '/_config.inc.php';
+
 setlocale(LC_TIME, 'fr_FR');
 
-require_once 'Afup/AFUP_AppelConferencier.php';
+require_once AFUP_CHEMIN_RACINE . 'sources/Afup/AFUP_AppelConferencier.php';
 
 $forum_appel = new AFUP_AppelConferencier($bdd);
 $sessions = $forum_appel->obtenirListeSessionsPlannifies($config_forum['id']);
