33728ff229d5edc9d560de01d5a2651af2284dd0
Aurélien Vairet <afup@vairet.net>
2010-09-24 14:05:28 +0000

    Correction includes pour page resumes forum 2009

--- htdocs/pages/forumphp2009/resumes.php
+++ htdocs/pages/forumphp2009/resumes.php
@@ -1,9 +1,12 @@
 <?php
-require_once dirname(__FILE__) .'/../../../sources/Afup/Bootstrap/Http.php';
+//require_once dirname(__FILE__) .'/../../../sources/Afup/Bootstrap/Http.php';
+//require_once dirname(__FILE__) . '/_config.inc.php';
+require_once '../../include/prepend.inc.php';
 require_once dirname(__FILE__) . '/_config.inc.php';
+
 setlocale(LC_TIME, 'fr_FR');
 
-require_once 'Afup/AFUP_AppelConferencier.php';
+require_once AFUP_CHEMIN_RACINE . 'classes/afup/AFUP_AppelConferencier.php';
 
 $forum_appel = new AFUP_AppelConferencier($bdd);
 $sessions = $forum_appel->obtenirListeSessionsAvecResumes($config_forum['id']);
