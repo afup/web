ba748dcc5ae86f357e4d8e4167b2c5dce5f214de
Perrick Penet-Avez <perrick@noparking.net>
2010-10-14 06:16:31 +0000

    Nettoyage d'un fichier

--- sources/Afup/AFUP_Web.php
+++ sources/Afup/AFUP_Web.php
@@ -3,10 +3,6 @@ require_once dirname(__FILE__).'/AFUP_Configuration.php';
 require_once dirname(__FILE__).'/AFUP_Base_De_Donnees.php';
 
 class AFUP_Web {
-    public $route = "";
-    public $content;
-    public $title;
-    
     function mettreAJour($update = false) {
     	if ($update === true) {
     		$command = "svn export --force --username ".$GLOBALS['conf']->obtenir('svn|username')." --password ".$GLOBALS['conf']->obtenir('svn|password');
