4d52ee278b5369d92479c12b3c901f9ead18b3c6
Christophe Villeneuve <hellosct1@gmail.com>
2011-01-14 22:44:54 +0000

    balance view page

---
+++ htdocs/pages/administration/compta_balance_view.php
@@ -0,0 +1,59 @@
+<?php
+
+$action = verifierAction(array('lister', 'editer','raccourci'));
+//$compte = verifierAction(array('espece','paypal','courant','livreta'));
+
+//$tris_valides = array('Date', 'Evenement', 'catégorie', 'Description');
+//$sens_valides = array('asc', 'desc');
+$smarty->assign('action', $action);
+
+if (isset($_GET['details']) && $_GET['details'])
+	$details=$_GET['details'];
+else
+	$details ="";
+
+
+require_once dirname(__FILE__).'/../../../sources/Afup/AFUP_Compta.php';
+$compta = new AFUP_Compta($bdd);
+
+if (isset($_GET['id_periode']) && $_GET['id_periode']) 
+	$id_periode=$_GET['id_periode'];
+else
+	$id_periode="";
+	 
+$id_periode = $compta->obtenirPeriodeEnCours($id_periode);
+$smarty->assign('id_periode', $id_periode);
+
+$listPeriode = $compta->obtenirListPeriode();
+$smarty->assign('listPeriode', $listPeriode );
+
+if ($action == 'lister') {
+	$periode_debut=$listPeriode[$id_periode-1]['date_debut'];
+	$periode_fin=$listPeriode[$id_periode-1]['date_fin'];
+
+	$smarty->assign('compteurLigne',1);
+	
+//	$balance = $compta->obtenirBalance('',$periode_debut,$periode_fin);
+/*	$smarty->assign('balance', $balance);
+	
+	$totalDepense = $compta->obtenirTotalBalance(1,$periode_debut,$periode_fin);
+	$smarty->assign('totalDepense', $totalDepense);
+	
+	$totalRecette = $compta->obtenirTotalBalance(2,$periode_debut,$periode_fin);
+	$smarty->assign('totalRecette', $totalRecette);
+	
+	$difMontant = $totalRecette - $totalDepense ;
+	$smarty->assign('difMontant', $difMontant);
+*/
+	if ($details!='')
+	{
+		$dataDetails = $compta->obtenirBalanceDetails($details,$periode_debut,$periode_fin);
+		$smarty->assign('dataDetails', $dataDetails);
+
+		$sousTotal = $compta->obtenirSousTotalBalance($details,$periode_debut,$periode_fin);
+		$smarty->assign('sousTotal', $sousTotal);		
+	}
+
+}
+
+?>
