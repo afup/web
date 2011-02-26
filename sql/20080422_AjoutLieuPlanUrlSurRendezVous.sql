5b654d00151e4ebb03b6e75d2bec2685569e45f3
Perrick Penet-Avez <perrick@noparking.net>
2011-02-26 11:36:12 +0000

    Suppression de toutes les étapes à aujourd'hui

--- sql/20080422_AjoutLieuPlanUrlSurRendezVous.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `afup_rendezvous` ADD `adresse` MEDIUMTEXT NOT NULL AFTER `lieu` ;
-ALTER TABLE `afup_rendezvous` ADD `plan` VARCHAR(255) NOT NULL AFTER `lieu` ;
-ALTER TABLE `afup_rendezvous` ADD `url` VARCHAR(255) NOT NULL AFTER `lieu` ;
--- sql/20080510_AjoutTags.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `afup_tags` (
-  `id` int(11) NOT NULL auto_increment,
-  `source` varchar(255) default NULL,
-  `id_source` int(11) default NULL,
-  `tag` varchar(255) default NULL,
-  `id_personne_physique` int(11) default NULL,
-  `date` int(10) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
