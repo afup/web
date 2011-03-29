e3660c411015c1120a150c67d214a9bdfeb56849
Christophe Villeneuve <hellosct1@gmail.com>
2011-03-29 19:58:56 +0000

    RV listing PHP

--- htdocs/pages/administration/rendez_vous_listing.php
+++ htdocs/pages/administration/rendez_vous_listing.php
@@ -11,13 +11,11 @@ $rendez_vous = new AFUP_Rendez_Vous($bdd);
 if ($action == 'listing') 
 {
 	$rendezvous = $rendez_vous->obtenir((int)$_GET['id']);
-print_r($rendezvous);
 	$list_ordre=" nom ";
     $inscrits = $rendez_vous->obtenirListeInscrits($rendezvous['id'], $list_ordre, FALSE);
-echo"<hr>";
-print_r($inscrits);
-//    $smarty->assign('lesrendezvous', $rendez_vous->obtenirListe());
-    $smarty->assign('rendezvous', $rendezvous);
+
+    $smarty->assign('lesrendezvous', $rendezvous->obtenirListe());
+    $smarty->assign('rendezvous', $rendezvous['titre']);
     $smarty->assign('inscrits', $inscrits);
 
 }    
