280d181e5760f0a07019a7611e7ffeb30c02c65f
Christophe Villeneuve <hellosct1@gmail.com>
2011-03-29 11:53:27 +0000

    listing modifié pour RV

--- htdocs/pages/administration/rendez_vous_listing.php
+++ htdocs/pages/administration/rendez_vous_listing.php
@@ -4,18 +4,19 @@ $action = verifierAction(array('listing'));
 //$tris_valides = array('i.date', 'i.nom', 'f.societe', 'i.etat');
 //$sens_valides = array( 'desc','asc' );
 $smarty->assign('action', $action);
-echo $action;
 
 require_once dirname(__FILE__).'/../../../sources/Afup/AFUP_Rendez_Vous.php';
 $rendez_vous = new AFUP_Rendez_Vous($bdd);
 
 if ($action == 'listing') 
 {
-	echo "*";
 	$rendezvous = $rendez_vous->obtenir((int)$_GET['id']);
+print_r($rendezvous);
 	$list_ordre=" nom ";
     $inscrits = $rendez_vous->obtenirListeInscrits($rendezvous['id'], $list_ordre, FALSE);
-    $smarty->assign('lesrendezvous', $rendez_vous->obtenirListe());
+echo"<hr>";
+print_r($inscrits);
+//    $smarty->assign('lesrendezvous', $rendez_vous->obtenirListe());
     $smarty->assign('rendezvous', $rendezvous);
     $smarty->assign('inscrits', $inscrits);
 
