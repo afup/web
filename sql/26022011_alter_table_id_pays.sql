e4b05b9be1e0f17bb56daa0bf8fc76b1153321b0
Aurélien Vairet <afup@vairet.net>
2011-03-31 11:45:57 +0000

    Correction du ALTER TABLE pour le pays dans annuairepro_MembreAnnuaire

--- sql/26022011_alter_table_id_pays.sql
+++ sql/26022011_alter_table_id_pays.sql
@@ -1,1 +1,2 @@
-ALTER TABLE annuairepro_membreannuaire ADD id_pays VARCHAR( 2 ) NOT NULL AFTER Zone
+ALTER TABLE annuairepro_MembreAnnuaire ADD id_pays VARCHAR( 2 ) NOT NULL DEFAULT 'FR' AFTER Zone;
+ALTER TABLE annuairepro_MembreAnnuaire_iso ADD id_pays VARCHAR( 2 ) NOT NULL DEFAULT 'FR' AFTER Zone;
