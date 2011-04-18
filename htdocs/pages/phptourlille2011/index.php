975c0c1f3ddde373eac2f4efda6293c7a0310e5a
Amélie <communication@afup.org>
2011-04-18 15:26:52 +0000

    Textes pour la page d'accueil (bis)

--- htdocs/pages/phptourlille2011/index.php
+++ htdocs/pages/phptourlille2011/index.php
@@ -18,16 +18,23 @@ if ($jours_avant_forum < 0) {
 $smarty->assign('alerte_avant_forum', $alerte_avant_forum);
 
 $info_importante['titre'] = "PHP Tour Lille 2011<br>les 24 et 25 novembre 2011";
-$info_importante['contenu'] = "<p>Le <strong>PHP Tour Lille 2011</strong> est officiellement annoncé pour les <strong>24 et 25 novembre 2011</strong> à <strong>Euratechnologies / Lille</strong> (France).</p>";
+$info_importante['contenu'] = "<p>Le <strong>PHP Tour Lille 2011</strong> est officiellement annoncé pour les <strong>24 et 25 novembre 2011</strong> à <strong>Euratechnologies / Lille</strong> (France).</p>
+
+<p>Nouvel événement annuel et itinérant lancé par l'AFUP, <strong>le PHP Tour se penche sur les problématiques et thématiques propres à la région d'accueil</strong>. Experts nationaux et internationaux animeront conférences, retours d'expérience et ateliers en lien avec le tissu économique local, pour aider la communauté PHP à parvenir au top de ses capacités ! </p>
+
+<p>Le PHP Tour Lille 2011 mettra notamment l'accent sur le <strong>commerce en ligne</strong>, <strong>l'intégration d'application hétérogènes au sein des systèmes d'informations</strong> et <strong>l'échelle du web</strong>. </p>
+ 
+<p>Évènement itinérant, mais ambition nationale: l'AFUP se donne pour mission de faire du PHP Tour un rendez-vous annuel incontournable pour les professionnels du monde PHP. Developpeurs, décideurs, journalistes, venez découvrir le PHP Tour Lille 2011 !</p>";
 $infos_importantes[] = $info_importante;
 
 $smarty->assign('infos_importantes', $infos_importantes);
 
 $actualites = array();
 
-$actualite['titre'] = "15 ans de PHP, 10 ans d'AFUP : un programme riche pour cette année 2010";
-$actualite['contenu'] = "<p></p>";
-$actualite['date'] = "03 septembre 2010";
+$actualite['titre'] = "L'appel à conférenciers est lancé !";
+$actualite['contenu'] = "<p>Expert PHP, devenez conférencier lors du PHP Tour Lille 2011, le nouvel événement itinérant de l'AFUP ! Vous êtes expert dans le domaine du commerce en ligne, vous avez des connaissances pointues dans le domaine de l'intégration d'application hétérogènes dans les systèmes d'informations, vous pouvez nous faire bénéficier d'un retour d'expérience et de vos outils et astuces pour résister aux montées en charge: venez partager vos connaissances, en solo ou en groupe ! </p>
+<p>Rendez-vous dans la rubrique « Appel à conférenciers » et remplissez le formulaire avant le 31 mai 2011, minuit. </p>";
+$actualite['date'] = "18 avril 2011";
 $actualites[] = $actualite;
 
 $smarty->assign('actualites', $actualites);
