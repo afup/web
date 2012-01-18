593cfdc8e02d921d50dc936302956acdca1f17fd
Jean-Marc Fontaine <jm@jmfontaine.net>
2012-01-18 10:46:38 +0000

    Suppression des fichiers du répertoire /htdocs/pages/forumphp car ce n'est plus qu'une redirection vers le Forum PHP Courant

--- htdocs/pages/forumphp/inscription_rappel.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__FILE__) .'/../../../sources/Afup/Bootstrap/Http.php';
-
-require_once 'Afup/AFUP_Inscriptions_Forum.php';
-$inscriptions = new AFUP_Inscriptions_Forum($bdd);
-
-$inscriptions->ajouterRappel($_POST['email']);
-$smarty->display('inscriptions_rappel.html');
-?>
--- htdocs/pages/forumphp/paybox_refuse.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__FILE__) .'/../../../sources/Afup/Bootstrap/Http.php';
-
-require_once 'Afup/AFUP_Inscriptions_Forum.php';
-$inscriptions = new AFUP_Inscriptions_Forum($bdd);
-$inscriptions->modifierEtatInscription($_GET['cmd'], AFUP_FORUM_ETAT_REFUSE);
-
-$smarty->display('paybox_refuse.html');
-?>
