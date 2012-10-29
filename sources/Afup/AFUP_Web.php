7f2f46ff8364ad56a1f38c4a8054f3ef0718e5b7
Mickael Perraud <mikaelkael.fr@gmail.com>
2012-10-29 16:26:28 +0000

    Adminsitration : nettoyage d'APC sur mise à jour SVN

--- sources/Afup/AFUP_Web.php
+++ sources/Afup/AFUP_Web.php
@@ -7,7 +7,9 @@ class AFUP_Web {
     	if ($update === true) {
     		$command = "svn export --force --username ".$GLOBALS['conf']->obtenir('svn|username')." --password ".$GLOBALS['conf']->obtenir('svn|password');
     		$command .= " ".$GLOBALS['conf']->obtenir('svn|url').$GLOBALS['conf']->obtenir('svn|web')." ".dirname(__FILE__)."/../../";
+            apc_clear_cache();
     		$output = shell_exec($command);
+            apc_clear_cache();
     		return true;
     	} else {
     		return false;
