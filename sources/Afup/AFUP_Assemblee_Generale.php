2b4690cfc7d30697c00d69efa3c348bc7619ea53
Mickael Perraud <mikaelkael.fr@gmail.com>
2013-01-16 23:36:04 +0000

    ADminstration : chgt texte par défaut de la convocation en AG

--- sources/Afup/AFUP_Assemblee_Generale.php
+++ sources/Afup/AFUP_Assemblee_Generale.php
@@ -225,14 +225,15 @@ class AFUP_Assemblee_Generale
 
     function preparerCorpsDuMessage($timestamp) {
     	    $corps = "La prochaine assemblée générale de l'AFUP aura lieu le " . date('d/m/Y', $timestamp) . ".\n\n";
-    	    $corps .= "Cette AG se tiendra de 20h à 23h dans la salle Madrid de la FIAP ";
-    	    $corps .= "(adresse : 30 rue Cabanis - 75014 Paris - métro Glacières).\n\n";
+    	    $corps .= "Cette AG se tiendra, à partir de 18h30, à la Maison des Associations Solidaires à Paris. ";
+    	    $corps .= "Elle est située au 10/18 rue des terres au curé, Paris XIIIème.\n\n";
 
     	    $corps .= "Merci de bien vouloir cliquer sur le lien ci-dessous : ";
     	    $corps .= "il nous sert d'accusé de réception de cette convocation, ";
     	    $corps .= "il vous permet d'indiquer votre présence ";
     	    $corps .= "et - le cas échéant - à qui vous souhaitez transmettre votre pouvoir.\n\n";
 
+            $corps .= "Nous vous rappelons que seuls les membres à jour de cotisation peuvent participer et voter lors de l'assemblée générale.\n\n";
     	    return $corps;
     }
 
