bdd9886186460d108e75d567e4751496489e438d
Mickael Perraud <mikaelkael.fr@gmail.com>
2013-10-17 20:15:54 +0000

    FORUM : activation du site mobile (m.afup.org)

--- htdocs/m/index.php
+++ htdocs/m/index.php
@@ -1,3 +1,3 @@
 <?php
 // Redirect pour line court pendant le Forum
-header('Location: http://afup.org/pages/phptournantes2012/mobile.php');
+header('Location: http://afup.org/pages/phptournantes2013/mobile.php');
