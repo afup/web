36bf3b02dd78452de049fb021305c4db310561bb
Olivier Mansour <olivier.mansour@m6.fr>
2014-04-27 14:20:02 +0200

    passage du site mobile sur le phptour Lyon

--- htdocs/m/index.php
+++ htdocs/m/index.php
@@ -1,3 +1,3 @@
 <?php
 // Redirect pour line court pendant le Forum
-header('Location: http://afup.org/pages/forumphp2013/mobile.php');
+header('Location: http://afup.org/pages/phptourlyon2014/mobile.php');
