d365ff1d66a9741fa6ddd569625faf4e855e24c2
Mickael Perraud <mikaelkael.fr@gmail.com>
2014-05-15 23:25:43 +0200

    Fourm2014 : création initiale sous-site

---
+++ htdocs/pages/forumphp2014/_config.inc.php
@@ -0,0 +1,36 @@
+<?php
+/**
+    TODO en plus de cette config :
+    Créer la ligne afup_forum et mettre l'id de la ligne dans $config_forum['id']
+    Il faut aussi verifier le contenu des template (rechercher la date de l'année précedente )
+    Modifer le pdf du formulaire papier dans "/site/templates/forumphpXXXX/inscription-forum.pdf"
+       à partir du doc dans "/sources/doc/inscription au forum.odt"
+    Modifer le pdf du dossier sponsors dans "/site/templates/forumphp2009/pdf/Forum-PHP-2009-dossier-sponsor.pdf"
+       à partir du doc dans "/sources/forum/2009/Forum-PHP-2009-dossier-sponsor.odt"
+ */
+// Param de configuration sur site du Forum PHP
+
+define('AFUP_CHEMIN_SOURCE', realpath(dirname(__FILE__) . '/../../classes/afup/'));
+date_default_timezone_set("Europe/Paris");
+ini_set('display_errors',  $conf->obtenir('divers|afficher_erreurs'));
+
+require_once dirname(__FILE__).'/../../../sources/Afup/AFUP_Forum.php';
+require_once dirname(__FILE__).'/../../../sources/Afup/AFUP_Forum_Coupon.php';
+require_once dirname(__FILE__).'/../../../sources/Afup/AFUP_Forum_Partenaires.php';
+$forums = new AFUP_Forum($bdd);
+$coupons = new AFUP_Forum_Coupon($bdd);
+$partenairesForum = new AFUP_Forum_Partenaires($bdd);
+
+$idForum = 11; // 11 = Forum PHP 2014
+$config_forum = $forums->obtenir($idForum);
+$config_forum['date_debut'] = strtotime($config_forum['date_debut']);
+$config_forum['date_fin'] = strtotime($config_forum['date_fin']);
+$detailsCoupon = array_values($coupons->obtenirCouponsForum($idForum));
+$config_forum['coupons'] = array_merge($detailsCoupon, array_map("strtolower",$detailsCoupon));
+
+$config_forum['project_ids'] = array();
+
+$smarty->assign('forum_annee', $config_forum['annee']);
+
+$partenaires = $partenairesForum->obtenirTousPartenairesForum($idForum);
+$smarty->assign('partenaires', $partenaires);
---
+++ htdocs/pages/forumphp2014/convocation_visiteurs.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../../include/prepend.inc.php';
+require_once dirname(__FILE__) . '/_config.inc.php';
+require_once dirname(__FILE__).'/../../../sources/Afup/AFUP_Inscriptions_Forum.php';
+
+$inscription_manager = new AFUP_Inscriptions_Forum($bdd) ;
+$md5_code = $_GET['id'] ;
+$inscrit = $inscription_manager->obtenirInscription($md5_code) ;
+
+if(empty($inscrit))
+{
+  header('Location: /pages/forumphp'.$config_forum['annee'].'/index.php') ;
+}
+
+$smarty->assign('inscrit', $inscrit) ;
+$smarty->display('convocation_visiteurs.html') ;
