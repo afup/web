efda54454c6794d41e9fca1be8a37f7cf7b21ae5
paxal <cpascal@igraal.com>
2014-06-17 13:35:35 +0200

    Site: Ajout de la pagination a l'actualite

---
+++ sources/Afup/AFUP_Pagination.php
@@ -0,0 +1,37 @@
+<?php
+
+class AFUP_Pagination
+{
+    private $page_courante;
+    private $nombre_elements_par_page;
+    private $nombre_elements_total;
+    private $genere_route;
+
+    public function __construct($page_courante, $nombre_elements_par_page, $nombre_elements_total, $genere_route)
+    {
+        $this->page_courante = $page_courante;
+        $this->nombre_elements_par_page = $nombre_elements_par_page;
+        $this->nombre_elements_total = $nombre_elements_total;
+        $this->genere_route = $genere_route;
+    }
+
+    public function __toString()
+    {
+        global $smarty;
+
+        $page_max = ceil($this->nombre_elements_total / $this->nombre_elements_par_page);
+        $pages = array();
+        for ($i=1; $i<=$page_max; $i++) {
+            $pages[] = $i;
+        }
+
+        $smarty->assign('pagination_page_courante', $this->page_courante);
+        $smarty->assign('pagination_pages', $pages);
+
+        $smarty->register_function('pagination_genere_route', $this->genere_route, false);
+
+        ob_start();
+        $smarty->display('pagination.html');
+        return ob_get_clean();
+    }
+}
---
+++ tests/unitaires/pagination.test.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once dirname(__FILE__) . '/config.dist.php';
+require_once dirname(__FILE__) . '/../../sources/Afup/Bootstrap/Simpletest/Unit.php';
+require_once 'Afup/AFUP_Pagination.php';
+require_once 'smarty/Smarty.class.php';
+
+$smarty = new Smarty();
+$smarty->template_dir = (dirname(__FILE__).'/../../htdocs/templates/site/');
+$smarty->compile_dir = (dirname(__FILE__).'/../../htdocs/cache/tests/');
+
+class tests_Pagination extends UnitTestCase {
+    function testPagination69par10() {
+        $this->doTest(69, 10, 7);
+    }
+
+    function testPagination70par10() {
+        $this->doTest(70, 10, 7);
+    }
+
+    function testPagination71par10() {
+        $this->doTest(71, 10, 8);
+    }
+
+    function testPagination71par25() {
+        $this->doTest(71, 25, 3);
+    }
+
+    function genere_route() {
+        return '';
+    }
+
+    function doTest($nombre_elements, $elements_par_page, $nombre_pages) {
+        $pagination = new AFUP_Pagination(1, $elements_par_page, $nombre_elements, array($this, 'genere_route'));
+        $html = $pagination->__toString();
+
+        $doc = new DOMDocument();
+        @$doc->loadHTML($html);
+
+        $lis = $doc->getElementsByTagName('li');
+
+        $this->assertEqual($nombre_pages+4, $lis->length);
+    }
+}
