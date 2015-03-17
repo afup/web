00c36fd34d73645703c9ef24ec1bf58166e17e15
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2015-03-17 23:21:39 +0100

    Add AFUP_Mail
    Use send method like this:
    
    ```php
    $mail = new AFUP_Mail();
    $receiver = array(
        'email' => 'test@example.org',
        'name' => 'John Doe'
    );
    $data = array(
        'varname' => 'content'
    );
    $parameters = array(
        'subject' => 'Hello World!'
    );
    if ($mail->send('template-identifier', $receiver, $data, $parameters)) {
        echo "email sent";
    } else {
        echo "email not sent";
    }
    ```

---
+++ sources/Afup/AFUP_Mail.php
@@ -0,0 +1,103 @@
+<?php
+
+require_once dirname(__FILE__) . '/AFUP_Configuration.php';
+require_once dirname(__FILE__) . '/../../dependencies/mandrill/Mandrill.php';
+
+/**
+ * Send emails via Mandrill
+ */
+class AFUP_Mail
+{
+
+    protected $_apiKey;
+    protected $_mandrill;
+    
+    /**
+     * Init the object by getting the Maindrill API key
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        // Get the API key
+        $this->_apiKey = $this->_getConfig()->obtenir('mandrill|key');
+    }
+
+    /**
+     * Retrieve the configuration
+     * @throws Exception
+     * @return AFUP_Configuration
+     */
+    protected function _getConfig()
+    {
+        if (!isset($GLOBALS['AFUP_CONF'])) {
+            throw new Exception("Please include configuration.");
+        }
+
+        return $GLOBALS['AFUP_CONF'];
+    }
+
+    /**
+     * Retrieve Mandrill
+     * @return \Mandrill
+     */
+    public function getMandrill()
+    {
+        if (null === $this->_mandrill) {
+            $this->_mandrill = new Mandrill($this->_apiKey);
+        }
+        return $this->_mandrill;
+    }
+
+    /**
+     * Send an email
+     * @see https://mandrillapp.com/api/docs/messages.php.html#method=send-template
+     *
+     * @param string $template Template identifier
+     * @param array $receiver Receiver's data like ['email' => 'foo@bar.baz', 'name' => 'John Doe']
+     * @param array $data Data to put in the email
+     * @param boolean $async See Mandrill::sendTemplate()
+     * @param string $ipPool See Mandrill::sendTemplate()
+     * @param string $sendAt See Mandrill::sendTemplate()
+     * @return bool TRUE on success, FALSE on failure
+     */
+    public function send($template, array $receiver, array $data = array(), array $parameters = array(), $async = false, $ipPool = null, $sendAt = null)
+    {
+        // Receiver
+        if (!isset($parameters['to'])) {
+            $parameters['to'] = array($receiver);
+        }
+
+        // Blind copy
+        if (!isset($parameters['bcc_address'])) {
+            $parameters['bcc_address'] = $this->_getConfig()->obtenir('mandrill|bcc');
+        }
+
+        // Vars in the content
+        $vars = array();
+        foreach (($receiver + $data) as $key => $value) {
+            $vars[] = array(
+                'name'    => $key,
+                'content' => $value,
+            );
+        }
+
+        // Send the email using template
+        try {
+            $resp = $this->getMandrill()->messages->sendTemplate(
+                $template,
+                $vars,
+                $parameters,
+                $async,
+                $ipPool,
+                $sendAt
+            );
+        } catch(Mandrill_Error $e) {
+            return false;
+        }
+
+        return ($resp[0]['status'] === 'sent');
+    }
+
+}
+
