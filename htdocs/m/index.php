85cec095dadf9e9c1b7b4e34fb9ae21e1553109d
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2015-04-07 22:09:20 +0200

    Update index.php
    Replace Forum PHP with PHP Tour!

--- htdocs/m/index.php
+++ htdocs/m/index.php
@@ -1,3 +1,3 @@
 <?php
 // Redirect pour line court pendant le Forum
-header('Location: http://afup.org/pages/forumphp2014/mobile.php');
+header('Location: http://afup.org/pages/phptourluxembourg2015/mobile.php');
