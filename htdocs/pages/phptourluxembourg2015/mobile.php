3f47820a78c91f4dba5d282ea5130cd0c826cca2
Mickael Perraud <mikaelkael.fr@gmail.com>
2015-04-09 21:38:43 +0200

    PHPTour : correction for mobile page

--- htdocs/pages/phptourluxembourg2015/mobile.php
+++ htdocs/pages/phptourluxembourg2015/mobile.php
@@ -18,7 +18,7 @@ foreach ($sessions as $index => $session) {
   $session['conferenciers'] = $forum_appel->obtenirConferenciersPourSession($session['session_id']);
   $session['journees'] = explode(" ", $session['journee']);
 
-  if ('23' == date('d', $session['debut'])) {
+  if ('12' == date('d', $session['debut'])) {
     $journees[0][] = $session;
   } else {
     $journees[1][] = $session;
