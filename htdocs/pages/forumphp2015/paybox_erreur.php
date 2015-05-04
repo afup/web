58de10f0b20af75be272d7cbc3a42a6ec535561c
Mikael Randy <mikael.randy@m6.fr>
2015-05-04 22:35:15 +0200

    Add: forumPHP 2015

---
+++ htdocs/pages/forumphp2015/paybox_erreur.php
@@ -0,0 +1,10 @@
+<?php
+require_once '../../include/prepend.inc.php';
+require_once dirname(__FILE__) . '/_config.inc.php';
+require_once dirname(__FILE__).'/../../../sources/Afup/AFUP_Inscriptions_Forum.php';
+
+$inscriptions = new AFUP_Inscriptions_Forum($bdd);
+$inscriptions->modifierEtatInscription($_GET['cmd'], AFUP_FORUM_ETAT_ERREUR);
+
+$smarty->display('paybox_erreur.html');
+?>
---
+++ htdocs/pages/forumphp2015/sessions.php
@@ -0,0 +1,27 @@
+<?php
+require_once '../../include/prepend.inc.php';
+require_once dirname(__FILE__) . '/_config.inc.php';
+
+setlocale(LC_TIME, 'fr_FR');
+
+require_once dirname(__FILE__).'/../../../sources/Afup/AFUP_AppelConferencier.php';
+
+$forum_appel = new AFUP_AppelConferencier($bdd);
+$sessions = $forum_appel->obtenirListeSessionsPlannifies($config_forum['id']);
+$journees = array(
+  'Jeudi 23 octobre 2014' => array(),
+  'Vendredi 24 octobre 2014' => array()
+);
+foreach ($sessions as $index => $session) {
+	$session['conferenciers'] = $forum_appel->obtenirConferenciersPourSession($session['session_id']);
+  $session['journees'] = explode(" ", $session['journee']);
+
+  if ('23' == date('d', $session['debut'])) {
+    $journees['Jeudi 23 octobre 2014'][] = $session;
+  } else {
+    $journees['Vendredi 24 octobre 2014'][] = $session;
+  }
+}
+
+$smarty->assign('journees', $journees);
+$smarty->display('sessions.html');
