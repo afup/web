fb9d53469d095f59bd7c7960d11faafe338227db
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2016-01-06 00:15:06 +0100

    [-TASK] Remove useless SQL :sweat:
    Not required here.

--- sql/20160105_alter_compta.sql
+++ sql/20160105_alter_compta.sql
@@ -7,11 +7,3 @@
 ALTER TABLE `compta`
   ADD COLUMN `comment` VARCHAR(255) DEFAULT NULL AFTER `description`;
 
--- STORY
---  As the account manager
---  I want to know if a line in compta requires a proof document
---  Because I want to be sure to give the correct export for the final accounting report.
-
--- Add `require_proof` column in compta to know which line requires a document as proof
-ALTER TABLE `compta`
-  ADD COLUMN `require_proof` TINYINT(1) DEFAULT 0 AFTER `idcompte`;
