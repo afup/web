2b7c837058bc079606bf3f881a77322e9047f4b4
Mickael Perraud <mikaelkael.fr@gmail.com>
2016-05-16 14:46:18 +0200

    Administration : on peut déployer une autre branche (pour recetter une nouveauté par ex)

--- sources/Afup/AFUP_Web.php
+++ sources/Afup/AFUP_Web.php
@@ -5,7 +5,9 @@ require_once dirname(__FILE__).'/AFUP_Base_De_Donnees.php';
 class AFUP_Web {
     function mettreAJour($update = false) {
     	if ($update === true) {
-            $command = "cd ".$GLOBALS['conf']->obtenir('git|local_repo')."; git stash; git pull origin master; git stash pop;";
+            $command = "cd ".$GLOBALS['conf']->obtenir('git|local_repo')."; git stash;";
+            $command .= "git checkout master; git pull origin;";
+            $command .= "git checkout ".$GLOBALS['conf']->obtenir('git|deployed_branch')."; git stash pop;";
             $command .= "git checkout-index -f -a --prefix=/".$GLOBALS['conf']->obtenir('git|local_export') . " ; ";
             $command .= "cd /".$GLOBALS['conf']->obtenir('git|local_export')."; composer install --no-dev";
             opcache_reset();
