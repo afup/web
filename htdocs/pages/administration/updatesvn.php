9a6fb577886bda2f41b0fe8fb9991a7a5d18d351
Xavier Leune <xleune@ccmbenchmark.com>
2016-05-18 23:48:06 +0200

    On passe par la session pour afficher la sortie

--- htdocs/pages/administration/updatesvn.php
+++ htdocs/pages/administration/updatesvn.php
@@ -9,12 +9,18 @@ if (!defined('PAGE_LOADED_USING_INDEX')) {
 require_once dirname(__FILE__).'/../../../sources/Afup/AFUP_Web.php';
 $web = new AFUP_Web($bdd);
 $update = false;
+
+if(isset($_SESSION['update_output']) === true) {
+	$smarty->assign('output', $_SESSION['update_output']);
+	unset($_SESSION['update_output']);
+}
+
 if (isset($_GET['update']) and $_GET['update'] == 'true') {
 	$update = true;
 	$result = $web->mettreAJour($update);
     if ($result['result'] === true ) {
 		AFUP_Logs::log('Mise à jour du site Web');
 		afficherMessage('Le site Web a été mis à jour', 'index.php?page=updatesvn');
+		$_SESSION['update_output'] = $result['output'];
     }
-	$smarty->assign('output', $result['output']);
 }
