e0da0d3735168002a46606e12bcf2590076974cd
Mickael Perraud <mikaelkael.fr@gmail.com>
2016-05-19 22:04:22 +0200

    Administration: deployment review

--- sources/Afup/AFUP_Web.php
+++ sources/Afup/AFUP_Web.php
@@ -5,10 +5,27 @@ require_once dirname(__FILE__).'/AFUP_Base_De_Donnees.php';
 class AFUP_Web {
     function mettreAJour($update = false) {
     	if ($update === true) {
-            $command = "cd ".$GLOBALS['conf']->obtenir('git|local_repo')."; git stash;";
-            $command .= "git checkout ".$GLOBALS['conf']->obtenir('git|deployed_branch')."; git pull origin; git stash pop;";
+            // Direction le dossier avec le repo git
+            $command = "cd ".$GLOBALS['conf']->obtenir('git|local_repo').";";
+            // Si modif local, on la stocke temporairement pour éviter les conflits
+            $command .= "git stash;";
+            // On récupère les éventuelles nouvelles branches distantes
+            $command .= "git fetch";
+            // On track la branche souhaitée dans une branche locale (même si déjà fait)
+            $command .= "git branch --track branch-name origin/".$GLOBALS['conf']->obtenir('git|deployed_branch')." 2> /dev/null;";
+            // On se déplace sur la branche souhaitée
+            $command .= "git checkout ".$GLOBALS['conf']->obtenir('git|deployed_branch').";";
+            // On la met à jour
+            $command .= "git pull origin;";
+            // On réapplique les modifs locales
+            $command .= "git stash pop;";
+            // On extrait la branche vers le dossier d'export en ajoutant et en écrasant les fichiers
             $command .= "git checkout-index -f -a --prefix=/".$GLOBALS['conf']->obtenir('git|local_export') . " ; ";
-            $command .= "cd /".$GLOBALS['conf']->obtenir('git|local_export')."; rm -rf htdocs/tmp/twig;" ;
+            // Direction le dossier exporté
+            $command .= "cd /".$GLOBALS['conf']->obtenir('git|local_export').";";
+            // Nettoyage cache TWIG
+            $command .= "rm -rf htdocs/tmp/twig;" ;
+            // MAJ composer
             $command .= "composer install --no-dev";
             opcache_reset();
     		$output = shell_exec($command);
