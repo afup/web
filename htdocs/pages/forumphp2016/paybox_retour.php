3b03f29a87376036fd8bee8c2b61a55237e900bd
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2016-09-17 10:20:35 +0200

    Allow all the paybox IPs
    ref #217

--- htdocs/pages/forumphp2016/paybox_retour.php
+++ htdocs/pages/forumphp2016/paybox_retour.php
@@ -12,7 +12,27 @@ require_once dirname(__FILE__) . '/../../../sources/Afup/Bootstrap/_Common.php';
 $forum_inscriptions = new Inscriptions($bdd);
 $forum_facturation = new Facturation($bdd);
 
-if (in_array($_SERVER['REMOTE_ADDR'], ['195.101.99.73', '195.101.99.76', '194.2.160.66', '194.2.122.158','195.25.7.146', '195.25.7.166', '194.2.122.190']) === false) {
+$payboxIps = [
+  '194.2.122.158',
+  '194.2.122.190',
+  '194.2.160.64',
+  '194.2.160.66',
+  '194.2.160.75',
+  '194.2.160.80',
+  '194.2.160.82',
+  '194.2.160.91',
+  '195.101.99.73',
+  '195.101.99.76',
+  '195.25.67.0',
+  '195.25.67.11',
+  '195.25.67.2',
+  '195.25.67.22',
+  '195.25.7.146',
+  '195.25.7.157',
+  '195.25.7.159',
+  '195.25.7.166',
+];
+if (in_array($_SERVER['REMOTE_ADDR'], $payboxIps) === false) {
     /// Ici sont rencensees les IP indiquées par paybox dans leur doc
     die('...');
 }
