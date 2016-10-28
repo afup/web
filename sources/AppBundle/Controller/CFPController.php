059ace5692c3da494e899e1d6d43c46d8b5b684f
Xavier Leune <xleune@ccmbenchmark.com>
2016-10-28 10:44:13 +0200

    [WIP] Multispeaker

--- sources/AppBundle/Controller/CFPController.php
+++ sources/AppBundle/Controller/CFPController.php
@@ -4,14 +4,20 @@
 namespace AppBundle\Controller;
 
 
+use Afup\Site\Utils\Mail;
 use AppBundle\CFP\PhotoStorage;
 use AppBundle\Form\SpeakerType;
+use AppBundle\Form\TalkInvitationType;
 use AppBundle\Form\TalkType;
 use AppBundle\Model\Event;
 use AppBundle\Model\Repository\SpeakerRepository;
+use AppBundle\Model\Repository\TalkInvitationRepository;
 use AppBundle\Model\Repository\TalkRepository;
 use AppBundle\Model\Talk;
+use AppBundle\Model\TalkInvitation;
+use CCMBenchmark\Ting\Driver\QueryException;
 use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -76,20 +82,87 @@ class CFPController extends EventBaseController
         if ($event->getDateEndCallForPapers() < new \DateTime()) {
             return $this->render(':event/cfp:closed.html.twig', ['event' => $event]);
         }
-        $talk = $this->get('ting')->get(TalkRepository::class)->getOneBy(['id' => $talkId, 'forumId' => $event->getId()]);
+
+        /**
+         * @var $talkRepository TalkRepository
+         */
+        $talkRepository = $this->get('ting')->get(TalkRepository::class);
+        /**
+         * @var $speakerRepository SpeakerRepository
+         */
+        $speakerRepository = $this->get('ting')->get(SpeakerRepository::class);
+
+        /**
+         * @var $talk Talk
+         */
+        $talk = $talkRepository->getOneBy(['id' => $talkId, 'forumId' => $event->getId()]);
 
         if ($talk === null) {
             throw $this->createNotFoundException(sprintf('Talk %i not found', $talkId));
         }
         $this->denyAccessUnlessGranted('edit', $talk);
 
-        $form = $this->createForm(TalkType::class, $talk);
-        $formResponse = $this->handleTalkForm($request, $event, $form);
+        $talkForm = $this->createForm(TalkType::class, $talk);
+        $formResponse = $this->handleTalkForm($request, $event, $talkForm);
         if ($formResponse instanceof Response) {
             return $formResponse;
         }
+        $invitation = new TalkInvitation();
 
-        return $this->render(':event/cfp:propose.html.twig', ['event' => $event, 'form' => $form->createView(), 'talk' => $talk]);
+        $invitation
+            ->setSubmittedBy($this->getUser()->getId())
+            ->setSubmittedOn(new \DateTime())
+            ->setToken(base64_encode(random_bytes(30)))
+            ->setState(TalkInvitation::STATE_PENDING)
+            ->setTalkId($talkId)
+        ;
+
+        $invitationForm = $this->createForm(TalkInvitationType::class, $invitation);
+        $invitationForm->handleRequest($request);
+
+        if ($invitationForm->isSubmitted()) {
+            /**
+             * @var $invitation TalkInvitation
+             */
+            $invitation = $invitationForm->getData();
+
+            if ($invitationForm->isValid()) {
+                try {
+                    $this->get('ting')->get(TalkInvitationRepository::class)->save($invitation);
+                } catch (QueryException $exception) {
+                    $invitationForm->addError(new FormError($exception->getMessage()));
+                }
+                $user = $this->getUser();
+                // Send mail to the other guy, begging for him to join the talk
+                $text = <<<MAIL
+Bonjour !
+{$user->getLogin()} vous invite à rejoindre sa conférence "{$talk->getTitle()}" en tant que co-conférencier.
+Pour accepter, suivez ce lien: {$this->generateUrl('cfp_invite', ['eventSlug' => $eventSlug, 'talkId' => $talkId, 'token' => $invitation->getToken()])}
+Sinon, ignorez tout simplement cet email.
+
+L'équipe afup.
+MAIL;
+                $mail = new Mail();
+                $mail->sendSimpleMessage('CFP Afup', $text, [$invitation->getEmail()]);
+                dump($text);
+                die;
+            }
+        }
+
+        $invitations = $this->get('ting')->get(TalkInvitationRepository::class)->getPendingInvitationsByTalkId($talk->getId());
+        $speakers = $speakerRepository->getSpeakersByTalk($talk);
+
+        return $this->render(
+            ':event/cfp:edit.html.twig',
+            [
+                'event' => $event,
+                'form' => $talkForm->createView(),
+                'talk' => $talk,
+                'invitations' => $invitations,
+                'speakers' => $speakers,
+                'invitationForm' => $invitationForm->createView()
+            ]
+        );
     }
 
     public function proposeAction($eventSlug, Request $request)
@@ -120,6 +193,13 @@ class CFPController extends EventBaseController
 
         $talks = $this->get('ting')->get(TalkRepository::class)->getTalksBySpeaker($event, $this->get('app.speaker_factory')->getSpeaker($event));
         return $this->render(':event/cfp:sidebar.html.twig', ['talks' => $talks, 'event' => $event]);
+    }
+
+    public function inviteAction($eventSlug, $talkId, $token)
+    {
+        $event = $this->checkEventSlug($eventSlug);
+
+        return $this->render(':event/cfp:closed.html.twig');
     }
 
     /**
