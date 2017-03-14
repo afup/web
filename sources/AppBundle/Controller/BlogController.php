4031160b64a051db3acd746f619c40aa3cc75367
Xavier Leune <xleune@ccmbenchmark.com>
2017-03-14 07:59:19 +0100

    Page speakers pour le blog

--- sources/AppBundle/Controller/BlogController.php
+++ sources/AppBundle/Controller/BlogController.php
@@ -3,6 +3,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Event\Model\Repository\SpeakerRepository;
 use AppBundle\Event\Model\Repository\TalkRepository;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -23,5 +24,22 @@ class BlogController extends EventBaseController
         $talks = $talkRepository->getByEventWithSpeakers($event);
 
         return $this->render(':blog:program.html.twig', ['talks' => $talks, 'event' => $event]);
+    }
+
+    /**
+     * @param $eventSlug
+     * @return Response
+     */
+    public function speakersAction($eventSlug)
+    {
+        $event = $this->checkEventSlug($eventSlug);
+
+        /**
+         * @var $speakerRepository SpeakerRepository
+         */
+        $speakerRepository = $this->get('ting')->get(SpeakerRepository::class);
+        $speakers = $speakerRepository->getScheduledSpeakersByEvent($event);
+
+        return $this->render(':blog:speakers.html.twig', ['speakers' => $speakers, 'event' => $event]);
     }
 }
