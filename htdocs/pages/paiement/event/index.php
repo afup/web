a4595ec4e829513e03e4595f90d196d1acb5ee52
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2017-05-15 01:51:21 +0200

    Move payment pages

--- htdocs/pages/paiement/event/index.php
+++ htdocs/pages/paiement/event/index.php
@@ -1,9 +1,6 @@
 <?php
 use Afup\Site\Forum\Inscriptions;
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-//phpinfo();
-//exit;
+use Afup\Site\Forum\Forum;
 
 require_once dirname(__FILE__) .'/../../../../sources/Afup/Bootstrap/Http.php';
 
@@ -12,6 +9,14 @@ if (!isset($_GET['ref']) || !preg_match('`ins-([0-9]+)`', $_GET['ref'], $matches
 }
 
 $forum_inscriptions = new Inscriptions($bdd);
+$forum = new Forum($bdd);
+$forumId = isset($_GET['forum']) ? intval($_GET['forum']) : $forum->obtenirDernier();
+$forumData = $forum->obtenir($forumId);
+
+if (!isset($forumData['id']) || !$forumData['id']) {
+    die('Forum not found');
+}
+
 $ref = $matches[1];
 $inscription = $forum_inscriptions->obtenir($ref);
 
@@ -25,7 +30,7 @@ $paybox->set_rang($conf->obtenir('paybox|rang'));
 $paybox->set_identifiant('83166771');
 
 $paybox->set_total($prix * 100);
-$paybox->set_cmd('PHPTOUR17-' . $ref);
+$paybox->set_cmd($forumData['path'] . '-' . $ref);
 $paybox->set_porteur($inscription['email']);
 
 $paybox->set_effectue('http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/paybox_effectue.php');
@@ -43,6 +48,7 @@ $r[1] = preg_replace('#<b>.*?</b>#', '', $r[1]);
 $smarty->assign('paybox', str_ireplace('input type=submit', 'input type="submit" class="btn primary"', $r[1]));
 $smarty->assign('inscription', $inscription);
 $smarty->assign('original_ref', urlencode($_GET['ref']));
+$smarty->assign('forum', $forumData);
 $smarty->display('paybox_formulaire.html');
 
 
