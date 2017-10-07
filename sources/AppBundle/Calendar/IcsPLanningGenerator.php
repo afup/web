f616c3efb06dd2f885d4933a660f5e2f05971a2f
Adrien Gallou <adriengallou@gmail.com>
2017-10-07 11:01:07 +0200

    ajout d'un controlleur renvoyant le planning en ical

---
+++ sources/AppBundle/Calendar/IcsPLanningGenerator.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace AppBundle\Calendar;
+
+use AppBundle\Event\Model\Event;
+use AppBundle\Event\Model\Planning;
+use AppBundle\Event\Model\Repository\TalkRepository;
+use AppBundle\Event\Model\Room;
+use AppBundle\Event\Model\Talk;
+use Sabre\VObject\Component\VCalendar;
+
+class IcsPLanningGenerator
+{
+    /**
+     * @var TalkRepository
+     */
+    private $talkRepository;
+
+    public function __construct(TalkRepository $talkRepository)
+    {
+        $this->talkRepository = $talkRepository;
+    }
+
+    /**
+     * @param Event $event
+     * @return string
+     */
+    public function generateForEvent(Event $event)
+    {
+        $talks = $this->talkRepository->getByEventWithSpeakers($event);
+
+        $events = [];
+
+        $vcalendar = new VCalendar($events);
+        foreach ($talks as $talkWithData) {
+            /**
+             * @var $talk Talk
+             */
+            $talk = $talkWithData['talk'];
+
+            /**
+             * @var $planning Planning
+             */
+            $planning = $talkWithData['planning'];
+
+            /**
+             * @var $room Room
+             */
+            $room = $talkWithData['room'];
+
+            $vcalendar->add('VEVENT', [
+                'SUMMARY' => $talk->getTitle(),
+                'DTSTART' => $planning->getStart(),
+                'DTEND'   => $planning->getEnd(),
+                'DESCRIPTION' => $talk->getAbstract(),
+                'LOCATION' => $room->getName(),
+            ]);
+        }
+
+        return $vcalendar->serialize();
+    }
+}
