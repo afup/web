903c287f14fb2ef9843d215c594b777b4d9cc660
Xavier Leune <xleune@ccmbenchmark.com>
2017-10-10 09:11:20 +0200

    Premier test webpack

---
+++ sources/app.js
@@ -0,0 +1,30 @@
+var TableFilter = require('tablefilter');
+
+var tf = new TableFilter(document.querySelector('.tab--filterable'), {
+	base_path: 'node_modules/tablefilter/dist/tablefilter/',
+	filters_row_index: 1,
+	headers_row_index: 0,
+	col_1: 'select',
+	col_2: 'select',
+	col_3: 'select',
+	col_7: 'select',
+	col_8: 'none',
+	col_9: 'none',
+	theme: 'transparent',
+	alternate_rows: true,
+	rows_counter: true,
+	btn_reset: true,
+	loader: true,
+	status_bar: true,
+	col_types: [
+		{ type: 'date', locale: 'fr' },
+		'string',
+		'string',
+		'string',
+		'string',
+		{ type: 'formatted-number', decimal: ',', thousands: ' ' },
+		{ type: 'formatted-number', decimal: ',', thousands: ' ' },
+		'string'
+	],
+});
+tf.init();
