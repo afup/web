adbcdcf6001e50c3743832bd03f8a17bc74c2ef7
Adrien Gallou <adriengallou@gmail.com>
2017-10-14 19:23:27 +0200

    corrections sur la recherche des antennes
    * on pouvait atteindre la limite d'appel à l'api google maps,
    on passe donc par des appels avec une clef d'api, ce qui
    permet de faire des appels avec le niveau d'utilisation gratuite
    
    * au passage on rajoute un peu de cache pour limiter les appels
    
    * il y avait une adresse ne contenant que des tirets, on prends
    ce cas en compte

--- sources/AppBundle/Offices/OfficeFinder.php
+++ sources/AppBundle/Offices/OfficeFinder.php
@@ -9,9 +9,8 @@ use AppBundle\Event\Model\Event;
 use AppBundle\Event\Model\Invoice;
 use AppBundle\Event\Model\Repository\InvoiceRepository;
 use Geocoder\Exception\NoResult;
+use Geocoder\Geocoder;
 use Geocoder\Model\Coordinates;
-use Geocoder\Provider\GoogleMaps;
-use Ivory\HttpAdapter\CurlHttpAdapter;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 
 class OfficeFinder
@@ -37,12 +36,24 @@ class OfficeFinder
     private $officesCollection;
 
     /**
+     * @var Geocoder
+     */
+    private $geocoder;
+
+    /**
+     * @var array
+     */
+    private $geocodeCache = [];
+
+    /**
+     * @param Geocoder $geocoder
      * @param UserRepository $userRepository
      * @param InvoiceRepository $invoiceRepository
      * @param Inscriptions $inscriptions
      */
-    public function __construct(UserRepository $userRepository, InvoiceRepository $invoiceRepository, Inscriptions $inscriptions)
+    public function __construct(Geocoder $geocoder, UserRepository $userRepository, InvoiceRepository $invoiceRepository, Inscriptions $inscriptions)
     {
+        $this->geocoder = $geocoder;
         $this->userRepository = $userRepository;
         $this->invoiceRepository = $invoiceRepository;
         $this->inscriptions = $inscriptions;
@@ -99,29 +110,50 @@ class OfficeFinder
      */
     protected function findCoordinates(Invoice $invoice, User $user = null)
     {
-        $curl = new CurlHttpAdapter();
-        $geocoder = new GoogleMaps($curl);
-
         if (null !== $user) {
             if ($user->getCountry() != 'FR') {
                 return null;
             }
 
             try {
-                $adressCollection = $geocoder->geocode($user->getZipCode() . ' ' . $user->getCity());
+                $adressCollection = $this->geocode($user->getZipCode() . ' ' . $user->getCity());
             } catch (NoResult $noResult) {
-                $adressCollection = $geocoder->geocode($user->getCity());
+                $adressCollection = $this->geocode($user->getCity());
             }
         } else {
             try {
-                $adressCollection = $geocoder->geocode($invoice->getZipCode() . ' ' . $invoice->getCity());
+                $adressCollection = $this->geocode($invoice->getZipCode() . ' ' . $invoice->getCity());
             } catch (NoResult $noResult) {
-                $adressCollection = $geocoder->geocode($invoice->getCity());
+                $adressCollection = $this->geocode($invoice->getCity());
             }
+        }
+
+        if (null === $adressCollection) {
+            return null;
         }
 
         $address = $adressCollection->first();
         return $address->getCoordinates();
+    }
+
+    /**
+     * @param string $address
+     *
+     * @return \Geocoder\Model\AddressCollection
+     */
+    private function geocode($address)
+    {
+        $address = trim(trim($address), '-');
+
+        if (0 === strlen($address)) {
+            return null;
+        }
+
+        if (isset($this->geocodeCache[$address])) {
+            return $this->geocodeCache[$address];
+        }
+
+        return $this->geocodeCache[$address] = $this->geocoder->geocode($address);
     }
 
     /**
