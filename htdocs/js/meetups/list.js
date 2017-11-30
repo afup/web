464745f036500a7699d232f1061ee321c72f1b62
Adrien Gallou <adriengallou@gmail.com>
2017-11-30 21:54:49 +0100

    ajout d'une commande pour afficher les meetups

---
+++ htdocs/js/meetups/list.js
@@ -0,0 +1,167 @@
+var search = instantsearch({
+    appId: document.head.querySelector("[name=algolia_appid]").content,
+    apiKey: document.head.querySelector("[name=algolia_apikey]").content,
+    indexName: 'afup_meetups',
+    urlSync: {}
+});
+
+search.addWidget(
+    instantsearch.widgets.searchBox({
+        container: '#search-box',
+        placeholder: 'Rechercher un meetup...',
+        poweredBy: true
+
+    })
+);
+
+search.addWidget(
+    instantsearch.widgets.hits({
+        hitsPerPage: 7,
+        container: '#hits-container',
+        templates: {
+            empty: "Pas de résultat",
+            item: function(data) {
+                var content = ''
+                    + '<div class="container event-line">'
+                ;
+
+                content += ''
+                    + '<div class="col-md-1">'
+                    + '<img src="' + data.office.logo_url + '" />'
+                    + '</div>'
+                ;
+
+                content += ''
+                    + '<div class="col-md-2" style="text-align: center;font-size: 1.2em">'
+                    + data.office.label + '<br />'
+                    + '<span style="font-size: 1.2em">'
+                    + data.day_month
+                    + '<br />'
+                    + data.year
+                    + '</span>'
+                    + '</div>'
+                ;
+
+
+                content += ''
+                    + '<div class="col-md-8">'
+                    + '<div class="talk-list-title-container"><a href="' + data.event_url + '" ><h2>' + data.label + '</h2></a></div>'
+                ;
+
+                if ('undefined' !== typeof data.venue) {
+                    content += ''
+                        + data.venue.name
+                        + '<br />' + data.venue.address_1
+                        + '<br />' + data.venue.city
+                        + '<br />'
+                    ;
+                }
+                content += '<a href="#" class="description-toggler" data-toggled-html="Masquer la description">Voir la description</a>';
+                content += '<div class="event-description" style="display:none">' + data.description + '</div>';
+
+                content += ''
+                    + '</div>'
+                ;
+
+                content += ''
+                    + '</div>'
+                ;
+
+                return content;
+            }
+        }
+    })
+);
+
+
+var refinementItemTemplate = function(data) {
+    var content = "";
+    content += '<input type="checkbox" ';
+    if (data.isRefined) {
+        content += ' checked="checked" ';
+    }
+    content += " />";
+
+    content += '<label>' + data.name +'</label>';
+
+    if (data.count) {
+        content += ' <span class="talk-list-refinement-count-badge">' + data.count + "</span>";
+    }
+
+    return content;
+};
+
+
+search.addWidget(
+    instantsearch.widgets.refinementList({
+        container: '#refinement-office',
+        attributeName: 'office.label',
+        operator: "and",
+        templates: {
+            header: "<h4>Antenne</h4>",
+            item: refinementItemTemplate
+        },
+        showMore: {
+            templates: {
+                active: '<a class="ais-show-more ais-show-more__inactive">Voir moins</a>',
+                inactive: '<a class="ais-show-more ais-show-more__inactive">Voir plus</a>'
+            }
+        }
+    })
+);
+
+
+search.addWidget(
+    instantsearch.widgets.refinementList({
+        container: '#refinement-year',
+        attributeName: 'year',
+        operator: "and",
+        templates: {
+            header: "<h4>Année</h4>",
+            item: refinementItemTemplate
+        },
+        showMore: {
+            limit: 20,
+            templates: {
+                active: '<a class="ais-show-more ais-show-more__inactive">Voir moins</a>',
+                inactive: '<a class="ais-show-more ais-show-more__inactive">Voir plus</a>'
+            }
+        }
+    })
+);
+
+search.addWidget(
+    instantsearch.widgets.clearAll({
+        container: '#refinement-clear',
+        templates: {
+            link: 'Supprimer les filtres'
+        },
+        autoHideContainer: true
+    })
+);
+
+search.addWidget(
+    instantsearch.widgets.pagination({
+        container: '#pagination',
+        labels : {
+            first: '<i class="fa fa-angle-double-left"></i>',
+            previous: '<i class="fa fa-angle-left"></i>',
+            next: '<i class="fa fa-angle-right"></i>',
+            last: '<i class="fa fa-angle-double-right"></i>'
+        }
+    })
+);
+search.start();
+
+$(document).ready(function() {
+    $('#hits-container').on('click', '.description-toggler', function(e) {
+        e.preventDefault();
+        var toggler = $(this);
+        var line = toggler.parents('.event-line');
+        var description = $('.event-description', line);
+        description.toggle();
+        var togglerHtml = toggler.html();
+        toggler.html(toggler.data('toggled-html'))
+        toggler.data('toggled-html', togglerHtml)
+    });
+});
