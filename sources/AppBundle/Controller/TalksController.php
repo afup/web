87dba68fc899e8b427a1103c5ac03c0b1925a3a3
Adrien Gallou <adriengallou@gmail.com>
2018-01-14 22:00:25 +0100

    on affiche le message sur la bonne page

--- sources/AppBundle/Controller/TalksController.php
+++ sources/AppBundle/Controller/TalksController.php
@@ -6,18 +6,16 @@ use AppBundle\Event\Model\Repository\EventRepository;
 use AppBundle\Event\Model\Repository\PlanningRepository;
 use AppBundle\Event\Model\Repository\SpeakerRepository;
 use AppBundle\Event\Model\Repository\TalkRepository;
-use Symfony\Component\HttpFoundation\Request;
 
 class TalksController extends SiteBaseController
 {
-    public function listAction(Request $request)
+    public function listAction()
     {
         return $this->render(
             ':site:talks/list.html.twig',
             [
                 'algolia_app_id' => $this->getParameter('algolia_app_id'),
                 'algolia_api_key' => $this->getParameter('algolia_frontend_api_key'),
-                'source' => $request->get('src'),
             ]
         );
     }
