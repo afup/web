37ccc214214b5669b2d0bfe16fa837845444d991
Xavier Leune <xleune@ccmbenchmark.com>
2018-01-24 19:08:00 +0100

    WIP Amélioration stats membres

---
+++ sources/AppBundle/Controller/MembershipAdminController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Association\Model\Repository\UserRepository;
+use AppBundle\Association\Model\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class MembershipAdminController extends Controller
+{
+    public function reportingAction(Request $request)
+    {
+        $userRepository = $this->get('app.user_repository');
+
+        /**
+         * @var $users User[]
+         */
+        $users = $userRepository->getActiveMembers(UserRepository::USER_TYPE_ALL);
+
+        $companies = [];
+
+        $validCompanies = $validUsers = $companiesUsers = 0;
+        foreach ($users as $user) {
+            if ($user->isMemberForCompany()) {
+                $companiesUsers++;
+
+                if (isset($companies[$user->getCompanyId()]) === false) {
+                    $companies[$user->getCompanyId()] = true;
+                    $validCompanies++;
+                }
+
+            } else {
+                $validUsers++;
+            }
+        }
+
+        // Nombre de personnes morales en cours d'adhésion
+
+        // Nombre de personnes physiques rattachées à une personne morale en cours d'adhésion
+
+        // Nombre de personnes physiques en cours d'adhésion
+
+        // Evolution nombre de personnes physiques en cours d'adhésion
+
+        // Evolution sur 1 an:
+        // Calculer le nombre au début de l'année
+        // puis on fait une requete pour les nouvelles cotisations par jour & les périmées par jour
+
+        dump([$validUsers, $validCompanies, $companiesUsers]);die;
+    }
+}
