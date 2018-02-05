7758f4cb7f37f3e80b623e528494c0a7069da823
Adrien Gallou <adriengallou@gmail.com>
2018-02-05 19:00:32 +0100

    ajout de la version de certains assets dans l'url afin de limiter les problèmes de cache

---
+++ sources/AppBundle/Twig/AssetsExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace AppBundle\Twig;
+
+class AssetsExtension extends \Twig_Extension implements \Twig_Extension_GlobalsInterface
+{
+    private $kernelRootDir;
+
+    public function __construct($kernelRootDir)
+    {
+        $this->kernelRootDir = $kernelRootDir;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('asset_md5_start', function ($url) {
+                $path = $this->kernelRootDir . '/../htdocs/' . $url;
+
+                return substr(md5_file($path), 0, 8);
+            }, ['is_safe' => ['html']])
+        ];
+    }
+
+    public function getName()
+    {
+        return 'assets';
+    }
+}
