0f5a7e9619a91367e8fe7dbc8b0683ef72252dbb
Xavier Leune <xleune@ccmbenchmark.com>
2018-03-22 15:13:25 +0100

    Liaison avec l'envoi vers mailchimp

--- sources/AppBundle/Controller/Admin/TechLetter/TechLetterGenerateController.php
+++ sources/AppBundle/Controller/Admin/TechLetter/TechLetterGenerateController.php
@@ -59,12 +59,10 @@ class TechLetterGenerateController extends SiteBaseController
         $form = $this->createForm(GenerateType::class);
         $form->handleRequest($request);
 
-        if ($request->getMethod() === Request::METHOD_POST) {
-            $data = $form->getData();
-
-            /*$techletterGenerator = new Generator();
-            $techLetter = $techletterGenerator->generate($data['news'][0], $data['news'][1], array_filter($data['articles']), array_filter($data['projects']));
-            */
+        if (
+            $request->getMethod() === Request::METHOD_POST
+            && $this->isCsrfTokenValid('sendToMailchimp', $request->request->get('_csrf_token'))
+        ) {
             $mailContent = $this
                 ->render(
                     ':admin/techletter:mail_template.html.twig',
@@ -76,11 +74,11 @@ class TechLetterGenerateController extends SiteBaseController
                 ->getContent()
             ;
 
-            $subject = sprintf("Veille de l'AFUP du %s", date('d/m/Y'));
+            $subject = sprintf("Veille de l'AFUP du %s", $sending->getSendingDate()->format('d/m/Y'));
 
             $template = $this->get('app.mailchimp_techletter_api')->createTemplate($subject . ' - Template', $mailContent);
 
-            $this->get('app.mailchimp_techletter_api')->createCampaign(
+            $response = $this->get('app.mailchimp_techletter_api')->createCampaign(
                 $this->container->getParameter('mailchimp_techletter_list'),
                 [
                     'template_id' => $template->get('id'),
@@ -90,10 +88,13 @@ class TechLetterGenerateController extends SiteBaseController
                 ]
             );
 
-            $message = "La campagne a été générée. Il faut maintenant se connecter sur Mailchimp pour la valider/en planifier l'envoi";
+            $sending->setSentToMailchimp(true);
+            $sendingRepository->save($sending);
+
+            $message = "La campagne a été générée. Il faut maintenant <a href='https://us8.admin.mailchimp.com/campaigns/edit?id=" . $response['web_id'] ."' target='_blank'>se connecter sur Mailchimp</a> pour la valider/en planifier l'envoi";
             $this->addFlash('notice', $message);
 
-            return $this->redirectToRoute('admin_techletter_generate');
+            return $this->redirectToRoute('admin_techletter_index');
         }
 
         return $this->render(
@@ -102,46 +103,7 @@ class TechLetterGenerateController extends SiteBaseController
                 'title' => "Veille de l'AFUP",
                 'sending' => $sending,
                 'form' => $form->createView(),
-                'tech_letter' => $techLetter /*new Techletter\TechLetter(
-                    new Techletter\News('http://symfony.com/blog/symfony-4-0-4-released', 'Sortie de Symfony 4.0.4', new \DateTimeImmutable('2018-01-29')),
-                    null, //new Techletter\News('https://laravel-news.com/laravel-5-6', 'Sortie de Laravel 5.6', new \DateTimeImmutable('2018-02-07')),
-                    [
-                        new Techletter\Article(
-                            'https://localheinz.com/blog/2018/01/15/normalizing-composer.json/',
-                            'Normalizing composer.json - Andreas Möller',
-                            'localheinz.com',
-                            4,
-                            'If you are using composer, you have probably modified composer.json at least once to keep things nice and tidy...'
-                        ),
-                        new Techletter\Article(
-                            'https://mwop.net/blog/2018-01-23-psr-15.html',
-                            'PSR-15 - Matthew Weier O\'phinney',
-                            'mwop.net',
-                            9,
-                            'Yesterday, following a unanimous vote from its Core Committee, PHP-FIG formally accepted the proposed PSR-15, HTTP Server Handlers standard...'
-                        ),
-                        new Techletter\Article(
-                            'http://sergeyzhuk.me/2018/02/02/why-asynchronous-php/',
-                            'Asynchronous PHP: Why?',
-                            'sergeyzhuk.me',
-                            7,
-                            'Asynchronous programming is on demand today. Especially in web-development where responsiveness of the application plays a huge role...'
-                        ),
-                        new Techletter\Article(
-                            'https://murze.be/introducing-browsershot-v3-the-best-way-to-convert-html-to-pdfs-and-images',
-                            'Introducing Browsershot v3 - murze.be',
-                            'murze.be',
-                            5,
-                            'To convert html to a pdf or an image using wkhtmltopdf and wkhtmltoimage tends to be the popular option. Unfortunately those tools contain an outdated ...'
-                        ),
-                    ],
-                    [
-                        new Techletter\Project('https://github.com/angeloskath/php-nlp-tools', 'angeloskath/php-nlp-tools', 'Natural Language Processing Tools in PHP'),
-                        new Techletter\Project('https://github.com/eleme/geohash', 'eleme/geohash', 'php geohash encoder/decoder'),
-                        new Techletter\Project('https://github.com/novaway/elasticsearch-client', 'novaway/elasticsearch-client', 'A lightweight PHP 7.0+ client for Elasticsearch'),
-                        new Techletter\Project('https://github.com/jenssegers/date', 'jenssegers/date', 'A library to help you work with dates in multiple languages'),
-                    ]
-                )*/
+                'tech_letter' => $techLetter
             ]
         );
     }
@@ -231,6 +193,9 @@ class TechLetterGenerateController extends SiteBaseController
         }
         if ($sending->getSentToMailchimp() === true) {
             throw $this->createAccessDeniedException('You cannot edit a sent techletter');
+        }
+        if ($this->isCsrfTokenValid('techletterPreview', $request->request->get('_csrf_token')) === false) {
+            throw $this->createAccessDeniedException('You cannot edit this techletter');
         }
 
         $techletter = Techletter\TechLetterFactory::createTechLetterFromJson($request->request->get('techletter'));
