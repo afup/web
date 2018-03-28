b1dd1ce91b6294431adf37b78b24c77d3d592dc4
Xavier Leune <xleune@ccmbenchmark.com>
2018-03-28 18:45:08 +0200

    Retours sur PR

--- sources/AppBundle/Controller/Admin/TechLetter/TechLetterGenerateController.php
+++ sources/AppBundle/Controller/Admin/TechLetter/TechLetterGenerateController.php
@@ -4,22 +4,15 @@ namespace AppBundle\Controller\Admin\TechLetter;
 
 use AppBundle\Association\Model\Repository\TechletterSubscriptionsRepository;
 use AppBundle\Controller\SiteBaseController;
-use AppBundle\TechLetter\Form\GenerateType;
+use AppBundle\TechLetter\DataExtractor;
 use AppBundle\TechLetter\Form\SendingType;
-use AppBundle\TechLetter\HtmlParser;
 use AppBundle\TechLetter\Model as Techletter;
-use AppBundle\TechLetter\UrlCrawler;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class TechLetterGenerateController extends SiteBaseController
 {
-    /**
-     * @see https://fr.wikipedia.org/wiki/Mot_par_minute
-     */
-    const WORD_READ_BY_MINUTES = 250;
-
     public function indexAction(Request $request)
     {
         $repository = $this->get('app.techletter_sending_repository');
@@ -56,9 +49,6 @@ class TechLetterGenerateController extends SiteBaseController
         }
 
         $techLetter = Techletter\TechLetterFactory::createTechLetterFromJson($sending->getTechletter());
-
-        $form = $this->createForm(GenerateType::class);
-        $form->handleRequest($request);
 
         if (
             $request->getMethod() === Request::METHOD_POST
@@ -103,7 +93,6 @@ class TechLetterGenerateController extends SiteBaseController
             [
                 'title' => "Veille de l'AFUP",
                 'sending' => $sending,
-                'form' => $form->createView(),
                 'tech_letter' => $techLetter
             ]
         );
@@ -116,66 +105,8 @@ class TechLetterGenerateController extends SiteBaseController
             throw new BadRequestHttpException('Undefined url parameter');
         }
 
-        /**
-         * @todo create a specific class
-         */
-
-        $urlInfo = parse_url($url);
-
-        $urlCrawler = new UrlCrawler();
-        $html = $urlCrawler->crawlUrl($url);
-
-        $parser = new HtmlParser($html);
-
-        /**
-         * Certaines données sont représentées sous 2 titres car les différents modèles utilisent des noms différents pour
-         * des choses similaires
-         * @todo fix it
-         */
-        $data = [
-            'title' => substr($parser->getTitle(), 0, 250),
-            'name' => substr($parser->getTitle(), 0, 250),
-            'excerpt' => $parser->getMeta('description'),
-            'description' => $parser->getMeta('description'),
-            'host' => $urlInfo['host']
-        ];
-
-        $richSchema = $parser->getRichSchema();
-
-
-        $listOfTypes = [
-            "NewsArticle",
-            "Report",
-            "ScholarlyArticle",
-            "SocialMediaPosting",
-            "TechArticle",
-            "Article",
-            "BlogPosting",
-        ];
-
-        if ($richSchema !== false) {
-            foreach ($richSchema as $schema) {
-                if (
-                    ! isset($schema['@type'])
-                    || !in_array($schema["@type"], $listOfTypes)
-                ) {
-                    continue;
-                }
-
-                if (isset($schema['datePublished'])) {
-                    $date = new \DateTimeImmutable($schema['datePublished']);
-                    $data['date'] = $date->format('Y-m-d');
-                }
-                if (isset($schema['articleBody'])) {
-                    $body = strip_tags($schema['articleBody']);
-                    $data['readingTime'] = floor(str_word_count($body) / self::WORD_READ_BY_MINUTES);
-                }
-            }
-        }
-
-        $data = array_map(function ($value) {
-            return trim($value);
-        }, $data);
+        $dataExtractor = new DataExtractor();
+        $data = $dataExtractor->extractDataForTechLetter($url);
 
         return new JsonResponse($data);
     }
---
+++ sources/AppBundle/TechLetter/DataExtractor.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace AppBundle\TechLetter;
+
+class DataExtractor
+{
+    /**
+     * @see https://fr.wikipedia.org/wiki/Mot_par_minute
+     */
+    const WORD_READ_BY_MINUTES = 250;
+
+    public function extractDataForTechLetter($url)
+    {
+
+        $urlInfo = parse_url($url);
+
+        $urlCrawler = new UrlCrawler();
+        $html = $urlCrawler->crawlUrl($url);
+
+        $parser = new HtmlParser($html);
+
+        /**
+         * Certaines données sont représentées sous 2 titres car les différents modèles utilisent des noms différents pour
+         * des choses similaires
+         * @todo fix it
+         */
+        $data = [
+            'title' => substr($parser->getTitle(), 0, 250),
+            'name' => substr($parser->getTitle(), 0, 250),
+            'excerpt' => $parser->getMeta('description'),
+            'description' => $parser->getMeta('description'),
+            'host' => $urlInfo['host']
+        ];
+
+        $richSchema = $parser->getRichSchema();
+
+
+        $listOfTypes = [
+            "NewsArticle",
+            "Report",
+            "ScholarlyArticle",
+            "SocialMediaPosting",
+            "TechArticle",
+            "Article",
+            "BlogPosting",
+        ];
+
+        if ($richSchema !== false) {
+            foreach ($richSchema as $schema) {
+                if (
+                    ! isset($schema['@type'])
+                    || !in_array($schema["@type"], $listOfTypes)
+                ) {
+                    continue;
+                }
+
+                if (isset($schema['datePublished'])) {
+                    $date = new \DateTimeImmutable($schema['datePublished']);
+                    $data['date'] = $date->format('Y-m-d');
+                }
+                if (isset($schema['articleBody'])) {
+                    $body = strip_tags($schema['articleBody']);
+                    $data['readingTime'] = floor(str_word_count($body) / self::WORD_READ_BY_MINUTES);
+                }
+            }
+        }
+
+        $data = array_map(function ($value) {
+            return trim($value);
+        }, $data);
+
+        return $data;
+    }
+}
