b3cbdee1d121a5c723b7f533e48c4c4f8496dde6
Adrien Gallou <adriengallou@gmail.com>
2018-04-24 20:29:59 +0200

    on permet d'utiliser les tarifs spéciaux même après le sold out

--- sources/AppBundle/Event/Ticket/TicketTypeAvailability.php
+++ sources/AppBundle/Event/Ticket/TicketTypeAvailability.php
@@ -6,6 +6,7 @@ use AppBundle\Event\Model\Event;
 use AppBundle\Event\Model\Repository\TicketEventTypeRepository;
 use AppBundle\Event\Model\Repository\TicketRepository;
 use AppBundle\Event\Model\TicketEventType;
+use AppBundle\Event\Model\TicketType;
 
 class TicketTypeAvailability
 {
@@ -30,6 +31,11 @@ class TicketTypeAvailability
 
     public function getStock(TicketEventType $ticketEventType, Event $event)
     {
+        // Les tickets depuis les tokens doivent fonctionner même après le sold out
+        if ($ticketEventType->getTicketType()->getTechnicalName() == TicketType::SPECIAL_PRICE_TECHNICAL_NAME) {
+            return PHP_INT_MAX;
+        }
+
         if (count($ticketEventType->getTicketType()->getDays()) === 2) {
             // Two days ticket
             $stock = $event->getSeats() - max(
