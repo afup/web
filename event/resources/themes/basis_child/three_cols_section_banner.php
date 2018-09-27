eebc80197801c6814be2082de9281844f18ebec8
Adrien Gallou <adriengallou@gmail.com>
2018-09-27 22:59:24 +0200

    event : page builder on permet d'avoir des profile (trois colonnes) en mode banner
    Cela va permettre de créer des boutons en colonne sur toute la largeur
    qu'on utilisera pour l'AFUP Day.

---
+++ event/resources/themes/basis_child/three_cols_section_banner.php
@@ -0,0 +1,29 @@
+<?php
+
+$default = 1;
+$min = 1;
+$max = 20;
+
+$nthChild = $default;
+
+if (isset($_GET['nth-child']) && $_GET['nth-child'] >= $min && $_GET['nth-child'] <= $max) {
+    //die('a');
+    $nthChild = $_GET['nth-child'];
+}
+
+$css = <<<CSS
+.product-content-wrapper > div > section:nth-child($nthChild) {
+    background-image: url('/wp-content/uploads/header_afup-1.svg');
+    padding: 1.2rem 3.7rem;
+    margin: 2rem auto;
+    background-size: cover;
+    max-width: 100%;
+}
+
+.product-content-wrapper > div > section:nth-child($nthChild) .banner-button-container {
+    text-align: center;
+}
+CSS;
+
+header("Content-type: text/css");
+echo $css;
