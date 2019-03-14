94aaba9ef6d4ac33bd29044c1de2cdb7e281063d
macintoshplus <814683+macintoshplus@users.noreply.github.com>
2019-03-14 20:50:02 +0100

    retrait de l'include

--- sources/Afup/Planete/Planete_Billet.php
+++ sources/Afup/Planete/Planete_Billet.php
@@ -129,7 +129,6 @@ class Planete_Billet
             }
         }
 
-        //require_once 'htmlpurifier/HTMLPurifier.auto.php';
         $config = HTMLPurifier_Config::createDefault();
         $config->set('Core.Encoding', 'UTF-8');
         $config->set('HTML.Doctype', 'HTML 4.01 Transitional');
