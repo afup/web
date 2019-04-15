402d7326709da0ff2f93aaa113cc1b594686cb71
macintoshplus <macintoshplus@users.noreply.github.com>
2019-04-15 07:43:50 +0200

    merge master

--- htdocs/pages/forumphp2013/appel-a-conferenciers-en.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Afup\Site\Forum\AppelConferencier;
-
-require_once '../../include/prepend.inc.php';
-require_once dirname(__FILE__) . '/_config.inc.php';
-
-$smarty->display('fin_appel.html');
-?>
