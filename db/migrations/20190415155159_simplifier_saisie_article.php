d1231ea05c2a867aa79b52cf0893797ee149bc49
Jean-Baptiste Nahan <814683+macintoshplus@users.noreply.github.com>
2019-04-17 16:21:05 +0200

    Suppression d'une requête inutile

--- db/migrations/20190415155159_simplifier_saisie_article.php
+++ db/migrations/20190415155159_simplifier_saisie_article.php
@@ -7,7 +7,6 @@ class SimplifierSaisieArticle extends AbstractMigration
 {
     public function change()
     {
-        $this->execute("UPDATE afup_site_article SET chapeau=descriptif WHERE chapeau in null or chapeau = ''");
         $this->execute('ALTER TABLE afup_site_article DROP COLUMN surtitre');
         $this->execute('ALTER TABLE afup_site_article DROP COLUMN descriptif');
     }
