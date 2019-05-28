36c5f2e37a609e94b6dd80c8ebc0a0c3a923ca4d
Adrien Gallou <adriengallou@gmail.com>
2019-05-28 14:09:22 +0200

    changement de la room vers laquelle on publie les infos d'inscriptions

--- sources/AppBundle/Slack/MessageFactory.php
+++ sources/AppBundle/Slack/MessageFactory.php
@@ -176,7 +176,7 @@ class MessageFactory
         $inscriptionsData = $inscriptions->obtenirStatistiques($event->getId());
         $message = new Message();
         $message
-            ->setChannel('afupday2019')
+            ->setChannel('pole-forum')
             ->setUsername($event->getTitle() . ' - Inscriptions')
             ->setIconUrl('https://pbs.twimg.com/profile_images/600291061144145920/Lpf3TDQm_400x400.png')
         ;
