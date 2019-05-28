897d42d77efcc355541ece61259c0380db5663eb
Adrien Gallou <adriengallou@gmail.com>
2019-05-28 14:21:11 +0200

    modification de la room vers laquelle envoyer les stats du cfp

--- sources/AppBundle/Slack/MessageFactory.php
+++ sources/AppBundle/Slack/MessageFactory.php
@@ -232,7 +232,7 @@ class MessageFactory
     {
         $message = new Message();
         $message
-            ->setChannel('afupday2019')
+            ->setChannel('pole-forum')
             ->setUsername('CFP')
             ->setIconUrl('https://pbs.twimg.com/profile_images/600291061144145920/Lpf3TDQm_400x400.png')
         ;
