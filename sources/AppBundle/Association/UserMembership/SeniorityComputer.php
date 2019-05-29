ae3842e250b851d0e5bf2aab6e58e3a2a1917cc7
Adrien Gallou <adriengallou@gmail.com>
2019-05-29 13:31:25 +0200

    on passe le calcul de l'ancienneté dans une autre classe

---
+++ sources/AppBundle/Association/UserMembership/SeniorityComputer.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Association\UserMembership;
+
+use Afup\Site\Association\Cotisations;
+use AppBundle\Association\Model\User;
+
+class SeniorityComputer
+{
+
+    /**
+     * @var Cotisations
+     */
+    private $cotisations;
+
+    public function __construct(Cotisations $cotisations)
+    {
+        $this->cotisations = $cotisations;
+    }
+
+    public function compute(User $user)
+    {
+        $cotis = $this->cotisations->obtenirListe(AFUP_PERSONNES_PHYSIQUES, $user->getId());
+        $now = new \DateTime();
+        $diffs = [];
+
+        foreach ($cotis as $coti) {
+            $from = \DateTimeImmutable::createFromFormat('U', $coti['date_debut']);
+            $to = \DateTimeImmutable::createFromFormat('U', $coti['date_fin']);
+            $to = min($now, $to);
+            $diffs[] = $from->diff($to);
+        }
+
+        $reference = new \DateTimeImmutable();
+        $lastest = clone $reference;
+        foreach ($diffs as $dif) {
+            $lastest = $lastest->add($dif);
+        }
+
+        $totalDiffs = $reference->diff($lastest);
+
+        return $totalDiffs->y;
+    }
+
+}
