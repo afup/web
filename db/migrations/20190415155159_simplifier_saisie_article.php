2474ad2252b0245c0986adc4d7a0e9892d22cdcc
Adrien Gallou <adriengallou@gmail.com>
2019-08-31 20:58:10 +0200

    on récupère les anciens descriptifs quand on a pas de chapeau

--- db/migrations/20190415155159_simplifier_saisie_article.php
+++ db/migrations/20190415155159_simplifier_saisie_article.php
@@ -7,6 +7,7 @@ class SimplifierSaisieArticle extends AbstractMigration
 {
     public function change()
     {
+        $this->execute("update afup_site_article set chapeau = descriptif where LENGTH(TRIM(chapeau)) = 0 and LENGTH(TRIM(descriptif));");
         $this->execute('ALTER TABLE afup_site_article DROP COLUMN surtitre');
         $this->execute('ALTER TABLE afup_site_article DROP COLUMN descriptif');
     }
