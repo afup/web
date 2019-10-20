3de34d3f14b81b43b82636b1b6796a4efd8a38a3
Adrien Gallou <adriengallou@gmail.com>
2019-10-20 15:18:28 +0200

    on ne mentionne plus le PHP Tour dans les mails de relance

--- sources/AppBundle/Association/CompanyMembership/Reminder7DaysBeforeEnd.php
+++ sources/AppBundle/Association/CompanyMembership/Reminder7DaysBeforeEnd.php
@@ -17,7 +17,7 @@ qui ont permis à vos équipes de se perfectionner. Mais avant toute chose, par 
 participé au dynamisme de la communauté PHP, vous avez permis à des cycles de conférences majeurs d’exister et vous avez
 soutenu les valeurs et la philosophie de notre association.</p>
 
-<p><strong>Restez avec nous !</strong> De beaux projets sont en cours à l’AFUP et bien sûr un PHP Tour et un Forum PHP
+<p><strong>Restez avec nous !</strong> De beaux projets sont en cours à l’AFUP et bien sûr un AFUP Day et un Forum PHP
 sont à venir dans les mois prochains. Renouvelez dès maintenant votre adhésion, en réglant votre cotisation en ligne.
 Elle prendra effet dans 7 jours, au terme de l’adhésion actuelle. 
 </p>
--- sources/AppBundle/Association/UserMembership/Reminder7DaysBeforeEnd.php
+++ sources/AppBundle/Association/UserMembership/Reminder7DaysBeforeEnd.php
@@ -16,7 +16,7 @@ et vous avez pu bénéficier des tarifs spéciaux “membres AFUP” lors de nos
 par votre adhésion, vous avez activement participé au dynamisme de la communauté PHP, vous avez permis à des cycles de
 conférences majeurs d’exister, et vous avez soutenu les valeurs et la philosophie de notre association.</p>
 
-<p>Restez avec nous ! De beaux projets sont en cours à l’AFUP, et bien sûr un PHP Tour et un Forum PHP sont à venir
+<p>Restez avec nous ! De beaux projets sont en cours à l’AFUP, et bien sûr un AFUP Day et un Forum PHP sont à venir
 dans les mois prochains. Renouvelez dès maintenant votre adhésion, en réglant votre cotisation en ligne. Elle prendra
 effet dans 7 jours, au terme de l’adhésion actuelle. </p>
 
