2935bc574d1462f26ec92b4fa5b7c0cba89df8a0
Adrien Gallou <adriengallou@gmail.com>
2019-11-07 21:45:37 +0100

    suppression de statistiques en double

--- htdocs/pages/administration/statistiques.php
+++ htdocs/pages/administration/statistiques.php
@@ -9,20 +9,7 @@ if (!defined('PAGE_LOADED_USING_INDEX')) {
     exit;
 }
 
-
-$personnes_physiques = new Personnes_Physiques($bdd);
-$smarty->assign('membres_actifs'  , $personnes_physiques->obtenirNombreMembres(AFUP_DROITS_ETAT_ACTIF));
-$smarty->assign('membres_inactifs', $personnes_physiques->obtenirNombreMembres(AFUP_DROITS_ETAT_INACTIF));
-$smarty->assign('membres_total'   , $personnes_physiques->obtenirNombreMembres());
-
-$smarty->assign('personnes_physiques_actives'  , $personnes_physiques->obtenirNombrePersonnesPhysiques(AFUP_DROITS_ETAT_ACTIF));
-$smarty->assign('personnes_physiques_inactives', $personnes_physiques->obtenirNombrePersonnesPhysiques(AFUP_DROITS_ETAT_INACTIF));
-$smarty->assign('personnes_physiques_total'    , $personnes_physiques->obtenirNombrePersonnesPhysiques());
-
-
 $personnes_morales = new Personnes_Morales($bdd);
 $smarty->assign('personnes_morales_actives'  , $personnes_morales->obtenirNombrePersonnesMorales(AFUP_DROITS_ETAT_ACTIF));
 $smarty->assign('personnes_morales_inactives', $personnes_morales->obtenirNombrePersonnesMorales(AFUP_DROITS_ETAT_INACTIF));
 $smarty->assign('personnes_morales_total'    , $personnes_morales->obtenirNombrePersonnesMorales());
-
-?>
