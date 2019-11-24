0a98d638d966112b8d957be2d3e8f36a334ab2a6
Adrien Gallou <adriengallou@gmail.com>
2019-11-24 15:08:10 +0100

    formulaire d'ajout de basges pour un user

--- htdocs/pages/administration/personnes_physiques.php
+++ htdocs/pages/administration/personnes_physiques.php
@@ -269,6 +269,18 @@ if ($action == 'lister') {
         $userBadges = iterator_to_array($userBadgesRepository->findByUserId($_GET['id']));
     }
 
-    $smarty->assign('user_badges_content', $this->get('twig')->render('::admin/members/user_badge/user_badges.html.twig', ['user_badges' => $userBadges]));
+    $userBadgeForm = $this->createForm(
+        \AppBundle\Association\Form\UserBadgeType::class,
+        [],
+        [
+            'user' => $this->getUser(),
+            'action' => $this->generateUrl('admin_members_user_badge_new'),
+        ]
+    );
+
+    $smarty->assign('user_badges_content', $this->get('twig')->render('::admin/members/user_badge/user_badges.html.twig', [
+        'user_badges' => $userBadges,
+        'user_badge_form' => $userBadgeForm->createView(),
+    ]));
     $smarty->assign('formulaire', genererFormulaire($formulaire));
 }
---
+++ sources/AppBundle/Controller/Admin/Members/UserBadgeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Controller\Admin\Members;
+
+use AppBundle\Event\Model\Repository\UserBadgeRepository;
+use AppBundle\Event\Model\UserBadge;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserBadgeController extends Controller
+{
+    public function newAction(Request $request)
+    {
+        $userBadgeForm = $this->createForm(
+            \AppBundle\Association\Form\UserBadgeType::class,
+            [],
+            ['user' => $this->getUser()]
+        );
+
+        $userBadgeForm->handleRequest($request);
+
+        $data = $userBadgeForm->getData();
+
+        $userBadge = new UserBadge();
+        $userBadge->setBadgeId($data['badge']);
+        $userBadge->setIssuedAt($data['date']);
+        $userBadge->setUserId($data['user']);
+
+        $userBadgeRepository = $this->get('ting')->get(UserBadgeRepository::class);
+        $userBadgeRepository->save($userBadge);
+
+        return $this->redirect($request->headers->get('referer'));
+    }
+}
