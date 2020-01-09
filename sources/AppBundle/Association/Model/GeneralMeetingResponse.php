b52d2e6c55a0089338de225385aac9dfc27e7d0e
Adrien Gallou <adriengallou@gmail.com>
2020-01-09 13:04:59 +0100

    ajout du modèle pour accéder aux réponses de présences d'AG

---
+++ sources/AppBundle/Association/Model/GeneralMeetingResponse.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace AppBundle\Association\Model;
+
+use CCMBenchmark\Ting\Entity\NotifyProperty;
+use CCMBenchmark\Ting\Entity\NotifyPropertyInterface;
+
+class GeneralMeetingResponse implements NotifyPropertyInterface
+{
+    use NotifyProperty;
+
+    const STATUS_PENDING = AFUP_ASSEMBLEE_GENERALE_PRESENCE_INDETERMINE;
+    const STATUS_PRESENT = AFUP_ASSEMBLEE_GENERALE_PRESENCE_OUI;
+    const STATUS_NON_PRESENT = AFUP_ASSEMBLEE_GENERALE_PRESENCE_NON;
+
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     */
+    private $date;
+
+    /**
+     * @var int
+     */
+    private $userId;
+
+    /**
+     * @var int
+     */
+    private $status = self::STATUS_PENDING;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->propertyChanged('id', $this->id, $id);
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserid()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param int $userid
+     *
+     * @return $this
+     */
+    public function setUserId($userid)
+    {
+        $this->propertyChanged('manager', $this->userId, $userid);
+        $this->userId = $userid;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param \DateTime $submittedOn
+     *
+     * @return $this
+     */
+    public function setDate(\DateTime $date)
+    {
+        $this->propertyChanged('date', $this->date, $date);
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param int $status
+     *
+     * @return $this
+     */
+    public function setStatus($status)
+    {
+        $this->propertyChanged('status', $this->status, $status);
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function isPresent()
+    {
+        return self::STATUS_PRESENT === $this->getStatus();
+    }
+}
