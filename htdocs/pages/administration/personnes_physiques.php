1386d89aed39f137ebeca7975c8034007a1558f7
Adrien Gallou <adriengallou@gmail.com>
2020-03-21 12:21:50 +0100

    personnes physiques : par défaut on affichage seulement les personnes actives

--- htdocs/pages/administration/personnes_physiques.php
+++ htdocs/pages/administration/personnes_physiques.php
@@ -40,9 +40,15 @@ if ($action == 'lister') {
         $needsUpToDateMembership = true;
     }
 
+    $onlyDisplayActive = true;
+    if (isset($_GET['also_display_active'])) {
+        $onlyDisplayActive = null;
+    }
+
     // Mise en place de la liste dans le scope de smarty
-    $smarty->assign('personnes', $personnes_physiques->obtenirListe($list_champs, $list_ordre, $list_filtre, false, false, false, null, null, $needsUpToDateMembership));
+    $smarty->assign('personnes', $personnes_physiques->obtenirListe($list_champs, $list_ordre, $list_filtre, false, false, false, $onlyDisplayActive, null, $needsUpToDateMembership));
     $smarty->assign('needs_up_to_date_membersip_checkbox', $needsUpToDateMembership ? '1': '0');
+    $smarty->assign('also_display_active', null === $onlyDisplayActive);
 } elseif ($action == 'supprimer') {
     if ($personnes_physiques->supprimer($_GET['id'])) {
         Logs::log('Suppression de la personne physique ' . $_GET['id']);
