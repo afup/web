fbf79858aec2bd118e539102aaaf708c98f9896c
Adrien Gallou <adriengallou@gmail.com>
2020-03-28 19:21:28 +0100

    suppression code mort de l'ancien tablefilter

--- sources/admin.js
+++ sources/admin.js
@@ -1,35 +1,55 @@
 var TableFilter = require('tablefilter');
 
-window.TableFilter = TableFilter;
+$('table.afup-tab-filterable').each(function() {
 
-/* Deprécié, utiliser fup-tab-filterable */
-var tf = new TableFilter(document.querySelector('.tab--filterable'), {
-	base_path: '/js_dist/tablefilter/',
-	filters_row_index: 1,
-	headers_row_index: 0,
-	col_1: 'select',
-	col_2: 'select',
-	col_3: 'select',
-	col_7: 'select',
-	col_8: 'none',
-	col_9: 'none',
-	themes: [{
-		name: 'transparent'
-	}],
-	alternate_rows: true,
-	rows_counter: true,
-	btn_reset: true,
-	loader: true,
-	status_bar: true,
-	col_types: [
-		{ type: 'date', locale: 'fr' },
-		'string',
-		'string',
-		'string',
-		'string',
-		{ type: 'formatted-number', decimal: ',', thousands: ' ' },
-		{ type: 'formatted-number', decimal: ',', thousands: ' ' },
-		'string'
-	],
+    var tfConfig = {
+        auto_filter: false,
+        base_path: '/js_dist/tablefilter/',
+        filters_row_index: 1,
+        headers_row_index: 0,
+        themes: [{
+            name: 'transparent'
+        }],
+        alternate_rows: false,
+        clear_filter_text: "- Filtre -",
+        rows_counter: {
+            text: "Lignes : "
+        },
+        help_instructions: false,
+        btn_reset: false,
+        loader: false,
+        status_bar: false,
+        cell_parser: {
+            parse: function(o, cell, colIndex){
+                /* Si on a un select, un form dans la cellule, on ne la renvoie pas */
+                var selectsInCell = $('select', cell);
+                if (selectsInCell.length > 0) {
+                    return $('option:selected', selectsInCell).text();
+                }
+
+                return $(cell).text()
+            }
+        },
+    };
+
+    var colNumber = 0;
+    $('thead th', this).each(function () {
+        var tfType = $(this).data('tf-filter-type');
+        if (typeof tfType !== 'undefined') {
+            tfConfig['col_' + colNumber] = tfType;
+        } else {
+            tfConfig['col_' + colNumber] = 'none';
+        }
+
+        if ((typeof tfConfig.cell_parser.cols) === 'undefined') {
+            tfConfig.cell_parser.cols = [];
+        }
+
+        tfConfig.cell_parser.cols.push(colNumber);
+
+        colNumber++;
+    });
+
+    var tf = new TableFilter(this, tfConfig);
+    tf.init();
 });
-tf.init();
