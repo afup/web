2d2e9a45c0afb84ed509e3f60022357e27ba90d3
Adrien Gallou <adriengallou@gmail.com>
2020-05-06 08:08:19 +0200

    on ajoute une vue globale sur les AFUP Day sur l'accueil

--- htdocs/pages/administration/accueil.php
+++ htdocs/pages/administration/accueil.php
@@ -1,9 +1,50 @@
 <?php
 
 /** @var \AppBundle\Controller\LegacyController $this */
+
+use Afup\Site\Forum\Inscriptions;
+use AppBundle\Event\Model\Repository\EventRepository;
+use AppBundle\Event\Model\Repository\TicketEventTypeRepository;
+
 if (!defined('PAGE_LOADED_USING_INDEX')) {
     trigger_error("Direct access forbidden.", E_USER_ERROR);
     exit;
 }
 
 $smarty->assign('user_label', $this->getUser()->getLabel());
+
+$eventRepository = $this->get('ting')->get(EventRepository::class);
+$ticketEventTypeRepository = $this->get('ting')->get(TicketEventTypeRepository::class);
+$nextevents = $eventRepository->getNextEvents();
+
+$cards = [];
+
+if ($this->isGranted('ROLE_FORUM')) {
+    $inscriptions = new Inscriptions($GLOBALS['AFUP_DB']);
+    foreach ($nextevents as $event) {
+        $inscriptionsData = $inscriptions->obtenirStatistiques($event->getId());
+
+        $infos = [];
+
+        if ($event->lastsOneDay()) {
+            $infos['statistics']['entrées'] = $inscriptionsData['premier_jour']['inscrits'];
+        } else {
+            $infos['statistics']['entrées (premier jour)'] = $inscriptionsData['premier_jour']['inscrits'];
+            $infos['statistics']['entrées (deuxième jour)'] = $inscriptionsData['second_jour']['inscrits'];
+        }
+
+        $infos['event_name'] = $event->getTitle();
+        $infos['event_id'] = $event->getId();
+
+        $montantTotal = 0;
+        foreach ($ticketEventTypeRepository->getTicketsByEvent($event, false) as $ticketEventType) {
+            $montantTotal += $inscriptionsData['types_inscriptions']['payants'][$ticketEventType->getTicketTypeId()] * $ticketEventType->getPrice();
+        }
+
+        $infos['statistics']['montant total'] = number_format($montantTotal, 0, 0, ' ') . ' €';
+
+        $cards[] = $infos;
+    }
+}
+
+$smarty->assign('cards', $cards);
