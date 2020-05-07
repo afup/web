1e7b83bd0bc542b94ae7b1ce1392373fdd90ac9c
Mathieu Lemoine <kura@kura.ooo>
2020-05-07 21:39:33 +0200

    Refactoring du AdminSpeakerController pour être compatible Symfony 5

---
+++ sources/AppBundle/Controller/Admin/Speaker/ExportAction.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AppBundle\Controller\Admin\Speaker;
+
+use AppBundle\Event\Model\Repository\EventRepository;
+use AppBundle\Event\Speaker\ExportGenerator;
+use Assert\Assertion;
+use SplFileObject;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+
+class ExportAction
+{
+    /** @var EventRepository */
+    private $eventRepository;
+    /** @var ExportGenerator */
+    private $exportGenerator;
+
+    public function __construct(
+        EventRepository $eventRepository,
+        ExportGenerator $exportGenerator
+    ) {
+        $this->eventRepository = $eventRepository;
+        $this->exportGenerator = $exportGenerator;
+    }
+
+    public function __invoke(Request $request)
+    {
+        if ($request->query->has('id')) {
+            $event = $this->eventRepository->get($request->query->getInt('id'));
+        } else {
+            $event = $this->eventRepository->getNextEvent();
+        }
+        Assertion::notNull($event);
+        $file = new SplFileObject(sys_get_temp_dir() . DIRECTORY_SEPARATOR . uniqid('speaker_', true), 'w+');
+        $this->exportGenerator->export($event, $file);
+        $response = new BinaryFileResponse($file, BinaryFileResponse::HTTP_OK, ['Content-Type' => 'text/html; charset=utf-8']);
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, sprintf('speakers_%s_%s.csv', $event->getPath(), date('Ymd-His')));
+        $response->deleteFileAfterSend(true);
+
+        return $response;
+    }
+}
