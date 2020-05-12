beca3c82c1e3331680054c5f4e9e58919cff0ae9
Mathieu Lemoine <kura@kura.ooo>
2020-05-13 00:31:29 +0200

    migration forum 2006

--- htdocs/pages/forumphp/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('Location: http://www.forumphp.org/', true, 302);
-exit;
--- htdocs/pages/forumphp2006/inscription.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once dirname(__FILE__).'/../../../sources/Afup/Bootstrap/Http.php';
-
-$smarty->display('inscription.html');
