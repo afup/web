e683186518aa7b3646d474b27ef4404745edd871
macintoshplus <814683+macintoshplus@users.noreply.github.com>
2020-07-10 23:07:29 +0200

    migration liste des événements + nettoyage du code legacy

---
+++ sources/AppBundle/Controller/Admin/Event/ListAction.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Controller\Admin\Event;
+
+
+use AppBundle\Event\Model\Repository\EventRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Twig\Environment;
+
+class ListAction
+{
+    /**
+     * @var EventRepository
+     */
+    private $eventRepository;
+    /**
+     * @var Environment
+     */
+    private $twig;
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    public function __construct(EventRepository $eventRepository, Environment $twig, SessionInterface $session)
+    {
+        $this->eventRepository = $eventRepository;
+        $this->twig = $twig;
+        $this->session = $session;
+    }
+
+    public function __invoke(Request $request)
+    {
+        //TODO : à supprimer quand les actions via le formulaire auront été migée
+        if (isset($_SESSION['flash']['message'])) {
+            $this->session->getFlashBag()->add('notice', $_SESSION['flash']['message']);
+        }
+        if (isset($_SESSION['flash']['erreur'])) {
+            $this->session->getFlashBag()->add('error', $_SESSION['flash']['erreur']);
+        }
+        unset($_SESSION['flash']);
+
+        $list = $this->eventRepository->getList();
+
+        return new Response($this->twig->render('admin/event/list.html.twig', ['events'=>$list]));
+    }
+}
