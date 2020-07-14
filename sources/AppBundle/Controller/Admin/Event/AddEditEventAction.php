8f26ca2a817da3333631f65c509fd7d0f5c69a73
macintoshplus <814683+macintoshplus@users.noreply.github.com>
2020-07-14 22:49:24 +0200

    ajout des events coupon et la modification d'un event

--- sources/AppBundle/Controller/Admin/Event/AddEditEventAction.php
+++ sources/AppBundle/Controller/Admin/Event/AddEditEventAction.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller\Admin\Event;
 
 use AppBundle\Event\Form\EventType;
 use AppBundle\Event\Model\Event;
+use AppBundle\Event\Model\Repository\EventCouponRepository;
 use AppBundle\Event\Model\Repository\EventRepository;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -36,11 +37,16 @@ class AddEditEventAction
      * @var UrlGeneratorInterface
      */
     private $urlGenerator;
+    /**
+     * @var EventCouponRepository
+     */
+    private $couponRepository;
 
     public function __construct(
         FormFactoryInterface $formFactory,
         Environment $twig,
         EventRepository $eventRepository,
+        EventCouponRepository $couponRepository,
         FlashBagInterface $flashBag,
         UrlGeneratorInterface $urlGenerator
     ) {
@@ -49,24 +55,45 @@ class AddEditEventAction
         $this->eventRepository = $eventRepository;
         $this->flashBag = $flashBag;
         $this->urlGenerator = $urlGenerator;
+        $this->couponRepository = $couponRepository;
     }
 
     public function __invoke(Request $request, $id)
     {
         $event = new Event();
+        $couponTxt = null;
+        $action = 'ajouté';
         if ($id !== null) {
             $event = $this->eventRepository->get($id);
             if ($event === null) {
                 $this->flashBag->add('error', 'Evénement introuvable');
                 return new RedirectResponse($this->urlGenerator->generate('admin_event_list'));
             }
+            $coupons = $this->couponRepository->couponsListForEvent($event);
+            $couponTxt = [];
+            foreach ($coupons as $coupon) {
+                if ($coupon->getText() === null || empty($coupon->getText()) === true) {
+                    continue;
+                }
+                $couponTxt[] = $coupon->getText();
+            }
+            $couponTxt = implode(',', $couponTxt);
+            $action = 'modifié';
         }
 
         $form = $this->formFactory->create(EventType::class, $event);
+        $form->get('coupons')->setData($couponTxt);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $this->eventRepository->save($event);
+
+            $couponsPost = explode(',', $form->get('coupons')->getData());
+            $couponsPost = array_map('trim', $couponsPost);
+            $this->couponRepository->changeCouponForEvent($couponsPost, $event);
+
+            $this->flashBag->add('notice', 'Evénement '.$action);
+            return new RedirectResponse($this->urlGenerator->generate('admin_event_list'));
         }
         return new Response($this->twig->render('admin/event/form.html.twig', ['form' => $form->createView()]));
     }
