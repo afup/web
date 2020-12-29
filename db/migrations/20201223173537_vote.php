a01564d43109303c4025baed4b5acd4eb52c0db9
Adrien Gallou <adriengallou@gmail.com>
2020-12-29 07:24:16 +0100

    améloration clarté du nom de la table

--- db/migrations/20201223173537_vote.php
+++ db/migrations/20201223173537_vote.php
@@ -7,9 +7,9 @@ class Vote extends AbstractMigration
     public function change()
     {
         $sql = <<<SQL
-DROP TABLE IF EXISTS `afup_assemblee_generale_vote`;
+DROP TABLE IF EXISTS `afup_assemblee_generale_question`;
 
-CREATE TABLE `afup_assemblee_generale_vote` (
+CREATE TABLE `afup_assemblee_generale_question` (
   `id` int(11) AUTO_INCREMENT,
   `date` int(11) unsigned NOT NULL,
   `label` varchar(255) NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE `afup_vote_assemblee_generale` (
   PRIMARY KEY (`afup_assemblee_generale_question_id`, `afup_personnes_physiques_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `afup_vote_assemblee_generale` ADD CONSTRAINT const_question FOREIGN KEY (`afup_assemblee_generale_question_id`) REFERENCES afup_assemblee_generale_vote (`id`);
+ALTER TABLE `afup_vote_assemblee_generale` ADD CONSTRAINT const_question FOREIGN KEY (`afup_assemblee_generale_question_id`) REFERENCES afup_assemblee_generale_question (`id`);
 SQL;
 
         $this->execute($sql);
