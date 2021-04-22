bafcddb3147c0ae72075334c3e64e4e429f97bcc
quentin <qgermain@gmail.com>
2021-04-22 19:03:03 +0200

    commonStart.php réinitialisé

--- sources/Afup/Bootstrap/commonStart.php
+++ sources/Afup/Bootstrap/commonStart.php
@@ -161,8 +161,8 @@ $services->get('ConnectionPool')->setConfig([
 $services
     ->get('MetadataRepository')
     ->batchLoadMetadata(
-        'AppBundle\Site\Model\Repository',
-        __DIR__ . '/../Admin/Site/Model/Repository/*.php',
+        'AppBundle\Event\Model\Repository',
+        __DIR__ . '/../Event/Model/Repository/*.php',
         ['default' => ['database' => $GLOBALS['AFUP_CONF']->obtenir('bdd|base')]]
     )
 ;
