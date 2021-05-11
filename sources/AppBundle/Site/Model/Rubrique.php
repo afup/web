95bc1c081edc5c49c69e8478ed43eb0b94bf72a2
quentin <qgermain@gmail.com>
2021-05-11 11:01:07 +0200

    fix braces

--- sources/AppBundle/Site/Model/Rubrique.php
+++ sources/AppBundle/Site/Model/Rubrique.php
@@ -44,7 +44,8 @@ class Rubrique implements NotifyPropertyInterface
 
     private $feuilleAssociee;
 
-    public function getId() {
+    public function getId()
+    {
         return $this->id;
     }
 
@@ -54,112 +55,136 @@ class Rubrique implements NotifyPropertyInterface
         $this->id = $id;
     }
 
-    public function getIdPersonnePhysique() {
+    public function getIdPersonnePhysique()
+    {
         return $this->idPersonnePhysique;
     }
 
-    public function setIdPersonnePhysique($id) {
+    public function setIdPersonnePhysique($id)
+    {
         $this->propertyChanged('idPersonnePhysique', $this->idPersonnePhysique, $id);
         $this->idPersonnePhysique = $id;
     }
 
-    public function getIdParent() {
+    public function getIdParent()
+    {
         return $this->idParent;
     }
 
-    public function setIdParent($id) {
+    public function setIdParent($id)
+    {
         $this->propertyChanged('idParent', $this->idParent, $id);
         $this->idParent = $id;
     }
 
-    public function getNom() {
+    public function getNom()
+    {
         return $this->nom;
     }
 
-    public function setNom($nom) {
+    public function setNom($nom)
+    {
         $this->propertyChanged('nom', $this->nom, $nom);
         $this->nom = $nom;
     }
 
-    public function getRaccourci() {
+    public function getRaccourci()
+    {
         return $this->raccourci;
     }
 
-    public function setRaccourci($raccourci) {
+    public function setRaccourci($raccourci)
+    {
         $this->propertyChanged('raccourci', $this->raccourci, $raccourci);
         $this->raccourci = $raccourci;
     }
 
-    public function getDescriptif() {
+    public function getDescriptif()
+    {
         return $this->descriptif;
     }
 
-    public function setDescriptif($descriptif) {
+    public function setDescriptif($descriptif)
+    {
         $this->descriptif = $descriptif;
         $this->propertyChanged('descriptif', $this->descriptif, $descriptif);
     }
 
-    public function getContenu() {
+    public function getContenu()
+    {
         return $this->contenu;
     }
 
-    public function setContenu($contenu) {
+    public function setContenu($contenu)
+    {
         $this->propertyChanged('contenu', $this->contenu, $contenu);
         $this->contenu = $contenu;
     }
 
-    public function getPosition() {
+    public function getPosition()
+    {
         return $this->position;
     }
 
-    public function setPosition($position) {
+    public function setPosition($position)
+    {
         $this->propertyChanged('position', $this->position, $position);
         $this->position = $position;
     }
 
-    public function getIcone() {
+    public function getIcone()
+    {
         return $this->icone;
     }
 
-    public function setIcone($icone) {
+    public function setIcone($icone)
+    {
         $this->propertyChanged('icone', $this->icone, $icone);
         $this->icone = $icone;
     }
 
-    public function getDate() {
+    public function getDate()
+    {
         return  new \DateTime (date('d-M-y', $this->date));
     }
 
-    public function setDate($date) {
+    public function setDate($date)
+    {
         $date = is_null($date) ? $date : $date->getTimestamp();
         $this->propertyChanged('date', $this->date, $date);
         $this->date = $date;
     }
 
-    public function getEtat() {
+    public function getEtat()
+    {
         return $this->etat;
     }
 
-    public function setEtat($etat) {
+    public function setEtat($etat)
+    {
         $this->propertyChanged('etat', $this->etat, $etat);
         $this->etat = $etat;
     }
 
-    public function getPagination() {
+    public function getPagination()
+    {
         return $this->pagination;
     }
 
-    public function setPagination($pagination = 0) {
+    public function setPagination($pagination = 0)
+    {
         $pagination = is_null($pagination) ? 0 : $pagination;
         $this->propertyChanged('pagination', $this->pagination, $pagination);
         $this->pagination = $pagination;
     }
 
-    public function getFeuilleAssociee() {
+    public function getFeuilleAssociee()
+    {
         return $this->feuilleAssociee;
     }
 
-    public function setFeuilleAssociee($feuilleAssociee) {
+    public function setFeuilleAssociee($feuilleAssociee)
+    {
         $this->propertyChanged('feuilleAssociee', $this->feuilleAssociee, $feuilleAssociee);
         $this->feuilleAssociee = $feuilleAssociee;
     }
