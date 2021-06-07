77d064d71dff6a8b1958fae521c0bb951907f846
Xavier Leune <xleune@ccmbenchmark.com>
2021-06-07 09:30:47 +0200

    Update functions.php

--- event/resources/themes/hestia-pro_child/functions.php
+++ event/resources/themes/hestia-pro_child/functions.php
@@ -3,6 +3,8 @@
  * @package Hestia Pro Child
  */
 
+if ( !defined( 'ABSPATH' ) ) exit;
+
 /** Allow upload of SVG files **/
 
 function cc_mime_types($mimes) {
@@ -40,3 +42,29 @@ function add_custom_head() {
     }
 }
 add_filter('wp_head', 'add_custom_head');
+
+if ( !function_exists( 'hestia_child_parent_css' ) ):
+    function hestia_child_parent_css() {
+        wp_enqueue_style( 'hestia_child_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'bootstrap' ) );
+        if( is_rtl() ) {
+            wp_enqueue_style( 'hestia_child_parent_rtl', trailingslashit( get_template_directory_uri() ) . 'style-rtl.css', array( 'bootstrap' ) );
+        }
+
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'hestia_child_parent_css', 10 );
+
+/**
+ * Import options from the parent theme
+ *
+ * @since 1.0.0
+ */
+function hestia_child_get_parent_options() {
+    $hestia_mods = get_option( 'theme_mods_hestia-pro' );
+    if ( ! empty( $hestia_mods ) ) {
+        foreach ( $hestia_mods as $hestia_mod_k => $hestia_mod_v ) {
+            set_theme_mod( $hestia_mod_k, $hestia_mod_v );
+        }
+    }
+}
+add_action( 'after_switch_theme', 'hestia_child_get_parent_options' );
