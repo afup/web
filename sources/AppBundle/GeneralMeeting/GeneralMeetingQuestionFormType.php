3a518039749d771a48ff6c67e11588e88d5e4fdb
akester <albin.kester@gmail.com>
2021-11-17 12:53:27 +0100

    Issue #1111 code review

--- sources/AppBundle/GeneralMeeting/GeneralMeetingQuestionFormType.php
+++ sources/AppBundle/GeneralMeeting/GeneralMeetingQuestionFormType.php
@@ -5,7 +5,7 @@ namespace AppBundle\GeneralMeeting;
 use AppBundle\Association\Model\GeneralMeetingQuestion;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
@@ -16,12 +16,8 @@ class GeneralMeetingQuestionFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('label', TextareaType::class, [
+            ->add('label', TextType::class, [
                 'label' => 'Question',
-                'attr' => [
-                    'rows' => 5,
-                    'cols' => 50,
-                ],
                 'constraints' => [
                     new NotBlank(),
                     new Length(['min' => 5, 'max' => 255])
