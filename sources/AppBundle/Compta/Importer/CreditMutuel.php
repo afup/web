efc5cc13c3d1fa6081ab278d1a307f5d52b49129
Sébastien Rogier <sebrogier@gmail.com>
2022-03-19 12:18:06 +0100

    Gestion de l'import du fichier d'opération bancaires pour le crédit mutuel

--- sources/AppBundle/Compta/Importer/CreditMutuel.php
+++ sources/AppBundle/Compta/Importer/CreditMutuel.php
@@ -8,8 +8,16 @@ class CreditMutuel implements Importer
 {
     const CODE = 'CMUT';
 
+    /**
+     * @var \SplFileObject
+     */
+    private $file;
+
     public function initialize($filePath)
     {
+        $this->file = new \SplFileObject($filePath, 'r');
+        $this->file->setCsvControl(';');
+        $this->file->setFlags(\SplFileObject::READ_CSV | \SplFileObject::SKIP_EMPTY);
     }
 
     /**
@@ -17,6 +25,17 @@ class CreditMutuel implements Importer
      */
     public function validate()
     {
+        $this->file->rewind();
+        $firstLine = $this->file->current();
+
+        if (!is_array($firstLine)) {
+            return false;
+        }
+
+        if (count($firstLine) === 6 && $firstLine[1] === 'Date de valeur') {
+            return true;
+        }
+
         return false;
     }
 
@@ -25,7 +44,43 @@ class CreditMutuel implements Importer
      */
     public function extract()
     {
-        return [];
+        $nbLineByDate = [];
+        foreach ($this->file as $i => $data) {
+            if ($i === 0) {
+                // ignore header ligne
+                continue;
+            }
+
+            if (count($data) !== 6) {
+                continue;
+            }
+
+            $dateEcriture = implode('-', array_reverse(explode('/', $data[0])));
+            if (!isset($nbLineByDate[$dateEcriture])) {
+                $nbLineByDate[$dateEcriture] = 0;
+            }
+            $nbLineByDate[$dateEcriture]++;
+
+            $description = $data[4];
+            $description = implode(' ', array_filter(explode(' ', $description)));
+
+            if ('' === $data[3]) {
+                $montant = abs(str_replace(',', '.', $data[2]));
+                $type = Operation::DEBIT;
+            } else {
+                $montant = abs(str_replace(',', '.', $data[3]));
+                $type = Operation::CREDIT;
+            }
+
+            // on doit fournir un numéro d'opération unique, ce qui permet lorsqu'on réimporte un fichier
+            // de ne pas dupliquer les écritures.
+            // Malheureusement le cmut, contrairement  à la caisse d'epargne, n'a pas cette clé dans le fichier.
+            // Donc on va sortir ça avec un sha1 de la ligne et croiser les doigts pour que l'ordonnancement soit le même.
+            // En théorie ça devrait le faire, le solde faisant partie de l'export, mais il ne faut sous-estimer personne
+            $numeroOperation = substr(sha1(implode($data)), 0, 20);
+
+            yield new Operation($dateEcriture, $description, $montant, $type, $numeroOperation);
+        }
     }
 
     public function getCompteId()
---
+++ tests/units/AppBundle/Compta/Importer/CreditMutuel.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace AppBundle\Compta\Importer\test\units;
+
+use AppBundle\Compta\Importer\CreditMutuel as TestedClass;
+use AppBundle\Compta\Importer\Operation;
+
+class CreditMutuel extends \atoum
+{
+
+    public function testValidate()
+    {
+        $importerCmut = new TestedClass();
+        $importerCmut->initialize(__DIR__ . '/_data/cmut.csv');
+        $this->boolean($importerCmut->validate())->isTrue();
+
+
+        $importerCE = new TestedClass();
+        $importerCE->initialize(__DIR__ . '/_data/ce.csv');
+        $this->boolean($importerCE->validate())->isFalse();
+    }
+
+    public function testExtract()
+    {
+        $importer = new TestedClass();
+        $importer->initialize(__DIR__ . '/_data/cmut.csv');
+
+        $this->boolean(true)->isTrue;
+
+        /** @var Operation[] $operations */
+        $operations = iterator_to_array($importer->extract());
+        $this->phpArray($operations)
+            ->hasSize(3);
+
+        $this->object($currentOperation = $operations[0])
+            ->isInstanceOf(Operation::class)
+                ->string($currentOperation->getDateEcriture())->isEqualTo('2022-01-20')
+                ->string($currentOperation->getDescription())->isEqualTo('VIR REMBOURSEMENT LA POSTE')
+                ->string($currentOperation->getType())->isEqualTo(Operation::DEBIT)
+                ->string($currentOperation->getNumeroOperation())->isEqualTo('b9b9f6e4dc1f923f654d')
+                ->float($currentOperation->getMontant())->isEqualTo(4.79)
+        ;
+
+        $this->object($currentOperation = $operations[1])
+            ->isInstanceOf(Operation::class)
+            ->string($currentOperation->getDateEcriture())->isEqualTo('2022-02-22')
+            ->string($currentOperation->getDescription())->isEqualTo('VIR GOOGLE IRELAND LIMITED GG102QZGUA')
+            ->string($currentOperation->getType())->isEqualTo(Operation::CREDIT)
+            ->string($currentOperation->getNumeroOperation())->isEqualTo('6518215a495b0182e609')
+            ->float($currentOperation->getMontant())->isEqualTo(64.03)
+        ;
+
+        $this->object($currentOperation = $operations[2])
+            ->isInstanceOf(Operation::class)
+            ->string($currentOperation->getDateEcriture())->isEqualTo('2022-03-03')
+            ->string($currentOperation->getDescription())->isEqualTo('VIR FACTURE 22010177')
+            ->string($currentOperation->getType())->isEqualTo(Operation::DEBIT)
+            ->string($currentOperation->getNumeroOperation())->isEqualTo('96ef77c410b75945ecc3')
+            ->float($currentOperation->getMontant())->isEqualTo(4592)
+        ;
+    }
+}
