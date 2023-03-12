72ac0b727016479cfc6633a6f67b16e69fcaa195
Vincent Amstoutz <vincent.amstoutz@outlook.fr>
2023-03-12 15:23:06 +0100

    fix: wrong method to migrate

--- db/migrations/20221211163812_rendez_vous_migration.php
+++ db/migrations/20221211163812_rendez_vous_migration.php
@@ -4,7 +4,7 @@ use Phinx\Migration\AbstractMigration;
 
 class RendezVousMigration extends AbstractMigration
 {
-    public function down()
+    public function up()
     {
         $this->execute('DROP TABLE afup_rendezvous, afup_rendezvous_inscrits, afup_rendezvous_slides');
     }
