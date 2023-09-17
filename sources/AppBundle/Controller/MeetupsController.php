11876bc9acf8a0d1fd111abded7e3a6157a4fdb8
Vincent Amstoutz <vincent.amstoutz@outlook.fr>
2023-09-17 09:43:31 +0200

    feat: switch from controller endpoint to sf command

--- sources/AppBundle/Controller/MeetupsController.php
+++ sources/AppBundle/Controller/MeetupsController.php
@@ -2,11 +2,7 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Event\Model\Repository\MeetupRepository;
-use AppBundle\Indexation\Meetups\MeetupScraper;
-use Exception;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class MeetupsController extends SiteBaseController
 {
@@ -20,40 +16,5 @@ class MeetupsController extends SiteBaseController
                 'source' => $request->get('src'),
             ]
         );
-    }
-
-    /**
-     * @throws Exception
-     *
-     * @return Response
-     */
-    public function scrapMeetupAction()
-    {
-        try {
-            $meetups = $this->getScrapper()->getEvents();
-
-            $meetupRepository = $this->get('ting')->get(MeetupRepository::class);
-
-            foreach ($meetups as $antenneMeetups) {
-                foreach ($antenneMeetups as $meetup) {
-                    $existingMeetup = $meetupRepository->get($meetup->getId());
-                    if (!$existingMeetup) {
-                        $meetupRepository->save($meetup);
-                    }
-                }
-            }
-            //TODO: à laisser ?
-            return new Response('Meetups scraped and saved successfully!');
-        } catch (\Exception $e) {
-            throw new \Exception('Problème lors du scraping ou de la sauvegarde des évènements Meetup', $e->getCode(), $e);
-        }
-    }
-
-    /**
-     * @return MeetupScraper
-     */
-    private function getScrapper()
-    {
-        return new MeetupScraper();
     }
 }
