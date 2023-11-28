606c24a7f19f354a2eda0a81c4e786c155091d59
Adrien Gallou <adriengallou@gmail.com>
2023-11-28 21:29:09 +0100

    correction typo sur calcul du montant de TVA

--- sources/Afup/Comptabilite/Comptabilite.php
+++ sources/Afup/Comptabilite/Comptabilite.php
@@ -173,7 +173,7 @@ class Comptabilite
         $requete .= 'compta_categorie.categorie, ';
         $requete .= 'compta_compte.nom_compte,    ';
         $requete .= '(compta.montant_ht_soumis_tva_0 + compta.montant_ht_soumis_tva_5_5 + compta.montant_ht_soumis_tva_10 + compta.montant_ht_soumis_tva_20) as montant_ht,   ';
-        $requete .= '((compta.montant_ht_soumis_tva_5_5*0.055) + (compta.montant_ht_soumis_tva_10*0.1) + compta.montant_ht_soumis_tva_20*0.2) as montant_tva,   ';
+        $requete .= '((compta.montant_ht_soumis_tva_5_5*0.055) + (compta.montant_ht_soumis_tva_10*0.1) + (compta.montant_ht_soumis_tva_20*0.2)) as montant_tva,   ';
         $requete .= 'compta.montant_ht_soumis_tva_0 as montant_ht_0,   ';
         $requete .= 'compta.montant_ht_soumis_tva_5_5 as montant_ht_5_5,   ';
         $requete .= 'compta.montant_ht_soumis_tva_5_5*0.055 as montant_tva_5_5,   ';
