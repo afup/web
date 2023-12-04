beed725300b841eced83065359f0baf768c9ea89
Albin <albin.kester@gmail.com>
2023-12-04 20:25:16 +0100

    Upgrade composer

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,7 +25,6 @@ class AppKernel extends Kernel
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
         }
 
         return $bundles;
