9afcc35434a4732e7298b50010cb36908e34d71b
Adrien Gallou <adriengallou@gmail.com>
2023-12-31 12:40:50 +0100

    ajout de données de test sur les factures

---
+++ db/seeds/Facture.php
@@ -0,0 +1,87 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class Facture extends AbstractSeed
+{
+    public function run()
+    {
+        $data = [
+            [
+                'id'    => '1',
+                'date_devis' => '2023-06-10',
+                'numero_devis' => '2023-01',
+                'date_facture' => '2023-06-11',
+                'numero_facture' => '2023-01',
+                'societe' => 'Krampouz',
+                'adresse' => '3, rue du port',
+                'code_postal' => '29000',
+                'ville' => 'Quimper',
+                'id_pays' =>  'FR',
+                'email' => 'mail@testmail.fr',
+                'tva_intra' => null,
+                'nom' => 'Le kellen',
+                'prenom' => 'Yan',
+                'etat_paiement' => 1,
+                'devise_facture' => 'EUR',
+                'ref_clt1' => 'Forum PHP 2023'
+            ],
+            [
+                'id'    => '2',
+                'date_devis' => '2024-01-03',
+                'numero_devis' => '2024-02',
+                'date_facture' => '2024-01-04',
+                'numero_facture' => '2023-02',
+                'societe' => 'Krampouz',
+                'adresse' => '3, rue du port',
+                'code_postal' => '29000',
+                'ville' => 'Quimper',
+                'id_pays' =>  'FR',
+                'email' => 'mail@testmail.fr',
+                'tva_intra' => null,
+                'nom' => 'Le kellen',
+                'prenom' => 'Yan',
+                'etat_paiement' => 1,
+                'devise_facture' => 'EUR',
+                'ref_clt1' => 'Forum PHP 2024'
+            ],
+        ];
+
+        $table = $this->table('afup_compta_facture');
+
+        $table->truncate();
+
+        $table
+            ->insert($data)
+            ->save()
+        ;
+
+        $dataDetails = [
+            [
+                'idafup_compta_facture' => '1',
+                'ref' => 'forum_php_2023',
+                'designation' => 'Forum PHP 2023 - Sponsoring Bronze',
+                'quantite' => '1',
+                'pu' =>  '1000',
+                'tva' => '0.00',
+            ],
+            [
+                'idafup_compta_facture' => '2',
+                'ref' => 'forum_php_2024',
+                'designation' => 'Forum PHP 2024 - Sponsoring Bronze',
+                'quantite' => '1',
+                'pu' =>  '1000',
+                'tva' => '0.00',
+            ],
+        ];
+
+        $table = $this->table('afup_compta_facture_details');
+
+        $table->truncate();
+
+        $table
+            ->insert($dataDetails)
+            ->save()
+        ;
+    }
+}
