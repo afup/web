be7dc55ea8148072268f20606037a42c5da33405
stakovicz <albin.kester@gmail.com>
2024-01-15 22:23:54 +0100

    !1416 bureau

--- configs/application/config-test.php
+++ configs/application/config-test.php
@@ -1,10 +1,3 @@
 <?php
 
-$configuration['bureau']['0']='MaximeTeneur';
-$configuration['bureau']['1']='JacquesBodinHullin';
-$configuration['bureau']['2']='ThierryMarianne';
-$configuration['bureau']['3']='paxal';
-$configuration['bureau']['4']='ChristopheVilleneuve';
-$configuration['bureau']['5']='LaurenceHoizey';
-
-return $configuration;
+return [];
