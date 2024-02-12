2ed8db8e3183152fee277494a55aa72e22172457
Stakovicz <83301974+stakovicz@users.noreply.github.com>
2024-02-12 20:35:28 +0100

    Revert "afup#1415 Session in DB"

--- htdocs/pages/planete/app.php
+++ htdocs/pages/planete/app.php
@@ -28,6 +28,8 @@ if ($_SERVER['HTTP_HOST'] === 'afup.dev' || $isDevEnv || $isTestEnv) {
         exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
     }
 
+    session_start();
+
     /** @var ClassLoader $loader */
     $loader = require __DIR__.'/../../../vendor/autoload.php';
     Debug::enable();
@@ -40,6 +42,8 @@ if ($_SERVER['HTTP_HOST'] === 'afup.dev' || $isDevEnv || $isTestEnv) {
 } else {
     /** @var ClassLoader $loader */
     $loader = require __DIR__.'/../../../vendor/autoload.php';
+
+    session_start();
 
     $kernel = new PlaneteAppKernel('prod', false);
 }
