0473a40d32272612a764239fdd894ee70d956d04
Raphael <raphael@widop.com>
2024-07-10 07:38:36 +0200

    fix/charter-page-inscription : #1139 - Déplace la page d'inscription dans la charte graphique du site non admin : 	* Déplace le code dans le SF 	* Fusionne les form_theme et l'applique sur le site SF 	* Supprime l'ancien code d'inscription 	* Fix les tests

---
+++ sources/AppBundle/Association/Form/RegisterUserType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Association\Form;
+
+use AppBundle\Association\Model\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegisterUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('userCommonInfo', UserType::class, [
+                'inherit_data' => true,
+                'data_class' => User::class,
+            ])
+            ->add('country', CountryType::class, [
+                'label' => 'Pays',
+                'required' => true
+            ])
+            ->add('mobilephone', TelType::class, [
+                'label' => 'Portable',
+                'required' => false
+            ])
+            ->add('nearestOffice', NearestOfficeChoiceType::class, [
+                'required' => false
+            ])
+            ->add('civility', ChoiceType::class, [
+                'choices' => ['M.' => User::CIVILITE_M, 'Mme' => User::CIVILITE_MME],
+                'required' => true
+            ])
+        ;
+
+        $builder->get('userCommonInfo')->add('phone', TextType::class, [
+            'required' => false
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
