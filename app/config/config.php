3a65230f0f02fef639423a035c32d8e41a65059f
Albin <albin@les-tilleuls.coop>
2024-07-16 21:14:16 +0200

    Configuration depuis les variables d'env en premier

--- app/config/config.php
+++ app/config/config.php
@@ -1,17 +1,23 @@
 <?php
 /**
- * On charge les parameters depuis le .env avec une conversion de nom en minuscule.
+ * On charge les parameters depuis le .env ou .env.dist avec une conversion de nom en minuscule.
  * À la migration vers Symfony 4+ ceci ne sera plus utile.
  * On fait ça à cause du bundle Ting qui ne peut utiliser les "%env()%" comme paramètre.
  */
-$lines = file(dirname(__FILE__, 3).'/.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$path = dirname(__FILE__, 3);
+$envFile = is_file($path.'/.env') ? $path.'/.env' : $path.'/.env.dist';
+$lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 foreach ($lines as $line) {
 
     if (strpos(trim($line), '#') === 0) {
         continue;
     }
 
-    list($name, $value) = explode('=', $line, 2);
+    list($name, $default) = explode('=', $line, 2);
+    // On va chercher dans les variables d'env en premier
+    if (!$value = getenv($name)) {
+        $value = $default;
+    }
     $name = strtolower(trim($name));
     $value = trim($value);
     if (is_numeric($value)) {
