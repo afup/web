02965b9285b8898096e03f3e5e904b902f9d7c5f
Stakovicz <albin@les-tilleuls.coop>
2024-11-07 22:04:14 +0100

    afup#1548 remove workshop afup day

--- db/seeds/Event.php
+++ db/seeds/Event.php
@@ -44,6 +44,36 @@ class Event extends AbstractSeed
                 'transport_information_enabled' => 1,
                 'has_prices_defined_with_vat' => 1,
             ],
+            [
+                'id' => 2,
+                'titre' => 'AFUP Day Lyon',
+                'path' => 'afup-day-lyon',
+                'logo_url' => 'http://78.media.tumblr.com/tumblr_lgkqc0mz9d1qfyzelo1_1280.jpg', // oui, c'est un chat
+                'nb_places' => 500,
+                'date_debut' => date('Y-m-d', $event),
+                'date_fin' => date('Y-m-d', $event + $oneDayInSeconds),
+                'annee' => date('Y', $event),
+                'text' => json_encode([
+                    'fr' => 'François le français',
+                    'en' => 'Henri l\'anglais',
+                    'sponsor_management_fr' => '**Sponsors**, venez, vous serez très visible !',
+                    'sponsor_management_en' => '**Sponsors**, come, you will be very visible!',
+                    'mail_inscription_content' => 'Contenu email',
+                ]),
+                'date_fin_appel_projet' => $now + $oneMonthInSeconds,
+                'date_fin_appel_conferencier' => $event - $oneMonthInSeconds * 2,
+                'date_fin_vote' => date('Y-m-d H:i:s', ($event - $oneMonthInSeconds * 2) + $oneDayInSeconds * 7),
+                'date_fin_prevente' => $now + $oneMonthInSeconds,
+                'date_fin_vente' => $event - $oneDayInSeconds * 7,
+                'date_fin_vente_token_sponsor' => $event - $oneDayInSeconds * 7,
+                'date_fin_saisie_repas_speakers' => $event - $oneDayInSeconds * 7,
+                'date_fin_saisie_nuites_hotel' => $event - $oneDayInSeconds * 7,
+                'place_name' => 'Paris',
+                'place_address' => 'Marriott Rive Gauche',
+                'date_annonce_planning' => date('Y-m-d H:i:s', $now - $oneMonthInSeconds),
+                'transport_information_enabled' => 1,
+                'has_prices_defined_with_vat' => 1,
+            ],
         ];
 
         $table = $this->table('afup_forum');
