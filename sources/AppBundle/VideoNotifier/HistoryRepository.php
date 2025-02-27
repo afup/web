d50ef0ea32d7666aeb9bed2d0c41a5d6f8f10a8e
Nathan Boiron <github@mopolo.net>
2025-02-27 20:43:04 +0100

    Système de post automatique sur Bluesky
    fixes #560

---
+++ sources/AppBundle/VideoNotifier/HistoryRepository.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppBundle\VideoNotifier;
+
+use AppBundle\Event\Model\Talk;
+use Doctrine\DBAL\Connection;
+
+final class HistoryRepository
+{
+    private Connection $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function insert(HistoryEntry $entry): void
+    {
+        $this->connection->createQueryBuilder()
+            ->insert('video_notifier_history')
+            ->values([
+                'talk_id' => '?',
+                'status_id_bluesky' => '?',
+                'status_id_mastodon' => '?',
+            ])
+            ->setParameters([
+                $entry->getTalkId(),
+                $entry->getStatusIdBluesky(),
+                $entry->getStatusIdMastodon(),
+            ])
+            ->execute();
+    }
+
+    /**
+     * @param array<Talk> $talks
+     * @return array<int, int>
+     */
+    public function getNumberOfStatusesPerTalk(array $talks): array
+    {
+        $rows = ($qb = $this->connection->createQueryBuilder())
+            ->from('video_notifier_history', 'h')
+            ->select('h.talk_id', 'COUNT(h.id) AS quantity')
+            ->where(
+                $qb->expr()->in('h.talk_id', array_map(fn (Talk $talk) => $talk->getId(), $talks))
+            )
+            ->groupBy('h.talk_id')
+            ->execute()
+            ->fetchAllAssociative();
+
+        $map = [];
+
+        foreach ($rows as $row) {
+            $map[$row['talk_id']] = $row['quantity'];
+        }
+
+        return $map;
+    }
+}
