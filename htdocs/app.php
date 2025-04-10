6aa11c48c14ace102ef42f04395a5d730997ea3f
Albin Kester <83301974+stakovicz@users.noreply.github.com>
2025-04-10 16:47:41 +0200

    On corrige le x-forwarded pour la génération des liens en https (#1719)

--- htdocs/app.php
+++ htdocs/app.php
@@ -45,6 +45,9 @@ if (false !== $ccReverseProxyIps) {
 Request::setTrustedProxies(
     $proxies,
     Request::HEADER_X_FORWARDED_FOR
+    | Request::HEADER_X_FORWARDED_HOST
+    | Request::HEADER_X_FORWARDED_PORT
+    | Request::HEADER_X_FORWARDED_PROTO
 );
 
 $response = $kernel->handle($request);
