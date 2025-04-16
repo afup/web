efade2f3566c88e6f4a01c467e972d3d613e29d9
Nathan Boiron <github@mopolo.net>
2025-04-17 01:05:28 +0200

    Ajout de types natifs à l'entité `Vote`

--- sources/AppBundle/Event/Model/Vote.php
+++ sources/AppBundle/Event/Model/Vote.php
@@ -11,39 +11,31 @@ use Symfony\Component\Validator\Constraints as Assert;
 class Vote implements NotifyPropertyInterface
 {
     use NotifyProperty;
-    /**
-     * @var int
-     */
-    private $id;
+
+    private int $id;
 
     /**
-     * @var int
      * @Assert\NotBlank()
      * @Assert\GreaterThan(0)
      */
-    private $sessionId;
+    private int $sessionId = 0;
 
     /**
-     * @var int
      * @Assert\NotBlank()
      * @Assert\GreaterThan(0)
      */
-    private $user;
+    private int $user = 0;
+
+    private ?string $comment = null;
 
     /**
-     * @var string
-     */
-    private $comment;
-
-    /**
-     * @var int
      * @Assert\NotBlank()
      * @Assert\GreaterThan(
      *     value = 0,
      *     message = "Please set a note !"
      * )
      */
-    private $vote = 0;
+    private int $vote = 0;
 
     private ?\DateTime $submittedOn = null;
 
@@ -51,112 +43,71 @@ class Vote implements NotifyPropertyInterface
 
     private ?Talk $talk = null;
 
-    /**
-     * @return int
-     */
-    public function getId()
+    public function getId(): int
     {
         return $this->id;
     }
 
-    /**
-     * @param int $id
-     * @return Vote
-     */
-    public function setId($id): self
+    public function setId(int $id): self
     {
         $this->propertyChanged('id', $this->id, $id);
         $this->id = $id;
         return $this;
     }
 
-    /**
-     * @return int
-     */
-    public function getSessionId()
+    public function getSessionId(): int
     {
         return $this->sessionId;
     }
 
-    /**
-     * @param int $sessionId
-     * @return Vote
-     */
-    public function setSessionId($sessionId): self
+    public function setSessionId(int $sessionId): self
     {
         $this->propertyChanged('sessionId', $this->sessionId, $sessionId);
         $this->sessionId = $sessionId;
         return $this;
     }
 
-    /**
-     * @return int
-     */
-    public function getUser()
+    public function getUser(): int
     {
         return $this->user;
     }
 
-    /**
-     * @param int $user
-     * @return Vote
-     */
-    public function setUser($user): self
+    public function setUser(int $user): self
     {
         $this->propertyChanged('user', $this->user, $user);
         $this->user = $user;
         return $this;
     }
 
-    /**
-     * @return string
-     */
-    public function getComment()
+    public function getComment(): ?string
     {
         return $this->comment;
     }
 
-    /**
-     * @param string $comment
-     * @return Vote
-     */
-    public function setComment($comment): self
+    public function setComment(string $comment): self
     {
         $this->propertyChanged('comment', $this->comment, $comment);
         $this->comment = $comment;
         return $this;
     }
 
-    /**
-     * @return int
-     */
-    public function getVote()
+    public function getVote(): int
     {
         return $this->vote;
     }
 
-    /**
-     * @param int $vote
-     * @return Vote
-     */
-    public function setVote($vote): self
+    public function setVote(int $vote): self
     {
         $this->propertyChanged('vote', $this->vote, $vote);
         $this->vote = $vote;
         return $this;
     }
 
-    /**
-     * @return \DateTime
-     */
     public function getSubmittedOn(): ?\DateTime
     {
         return $this->submittedOn;
     }
 
-    /**
-     * @return Vote
-     */
     public function setSubmittedOn(\DateTime $submittedOn): self
     {
         $this->propertyChanged('submittedOn', $this->submittedOn, $submittedOn);
@@ -164,34 +115,22 @@ class Vote implements NotifyPropertyInterface
         return $this;
     }
 
-    /**
-     * @return GithubUser
-     */
     public function getGithubUser(): ?GithubUser
     {
         return $this->githubUser;
     }
 
-    /**
-     * @return Vote
-     */
     public function setGithubUser(GithubUser $githubUser): self
     {
         $this->githubUser = $githubUser;
         return $this;
     }
 
-    /**
-     * @return Talk
-     */
     public function getTalk(): ?Talk
     {
         return $this->talk;
     }
 
-    /**
-     * @return Vote
-     */
     public function setTalk(Talk $talk): self
     {
         $this->talk = $talk;
