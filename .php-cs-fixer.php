5e91e2bd10f439aa9bf147e45e2a8f189a3560d1
Nathan Boiron <github@mopolo.net>
2025-05-18 22:41:55 +0200

    Passage du linter à PER-CS2.0
    Le standard PER est plus souple et évolutif que les PSR, car il change
    avec les modifications de PHP.
    
    https://www.php-fig.org/per/coding-style/

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -17,30 +17,20 @@ return (new PhpCsFixer\Config())
     ->registerCustomFixers(new PhpCsFixerCustomFixers\Fixers())
     ->setRiskyAllowed(true)
     ->setRules([
-        '@PSR2' => true,
+        '@PER-CS2.0' => true,
         '@PHP74Migration:risky' => true,
-        'blank_line_after_opening_tag' => true,
-        'concat_space' => ['spacing' => 'one'],
         'no_alias_functions' => true,
         'native_function_casing' => true,
-        'no_blank_lines_after_class_opening' => true,
-        'ordered_imports' => true,
         'phpdoc_no_access' => true,
-        'no_leading_import_slash' => true,
         'self_accessor' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'cast_spaces' => true,
         'no_unused_imports' => true,
-        'no_whitespace_in_blank_line' => true,
         'method_argument_space' => false,
         'statement_indentation' => false,
         MultilinePromotedPropertiesFixer::name() => [
             'keep_blank_lines' => true,
             'minimum_number_of_parameters' => 2,
         ],
-        'trailing_comma_in_multiline' => [
-            'elements' => ['arrays', 'parameters']
-        ],
+        'type_declaration_spaces' => true,
     ])
     ->setFinder($finder)
     ->setCacheFile(__DIR__.'/var/.php-cs-fixer.cache')
