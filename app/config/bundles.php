ffb35fd6788199d132ab7c5b3a5038c90d94e7a6
Stakovicz <albin@les-tilleuls.coop>
2025-06-09 22:20:10 +0200

    Mise à jour de twig/twig

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -6,6 +6,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     CCMBenchmark\TingBundle\TingBundle::class => ['all' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
