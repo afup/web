f9083d0d1c0917188bea9896cc325bfe8f44fdcc
Nathan Boiron <github@mopolo.net>
2025-06-21 11:47:57 +0200

    Uniformisation de l'instanciation du service Cotisations

--- sources/AppBundle/Association/CompanyMembership/SubscriptionManagement.php
+++ sources/AppBundle/Association/CompanyMembership/SubscriptionManagement.php
@@ -7,20 +7,17 @@ namespace AppBundle\Association\CompanyMembership;
 use Afup\Site\Association\Cotisations;
 use Afup\Site\Utils\Utils;
 use AppBundle\Association\Model\CompanyMember;
-use AppBundle\LegacyModelFactory;
 
-class SubscriptionManagement
+final readonly class SubscriptionManagement
 {
-    public function __construct(private readonly LegacyModelFactory $legacyModelFactory) {}
+    public function __construct(private Cotisations $cotisations) {}
 
     public function createInvoiceForInscription(CompanyMember $company, $numberOfMembers): array
     {
-        $subscription = $this->legacyModelFactory->createObject(Cotisations::class);
-
-        $endSubscription = $subscription->finProchaineCotisation(false);
+        $endSubscription = $this->cotisations->finProchaineCotisation(false);
 
         // Create the invoice
-        $subscription->ajouter(
+        $this->cotisations->ajouter(
             AFUP_PERSONNES_MORALES,
             $company->getId(),
             ceil($numberOfMembers / AFUP_PERSONNE_MORALE_SEUIL) * AFUP_COTISATION_PERSONNE_MORALE * (1 + Utils::MEMBERSHIP_FEE_VAT_RATE),
@@ -30,7 +27,7 @@ class SubscriptionManagement
             $endSubscription->format('U'),
             '',
         );
-        $subscriptionArray = $subscription->obtenirDerniere(AFUP_PERSONNES_MORALES, $company->getId());
+        $subscriptionArray = $this->cotisations->obtenirDerniere(AFUP_PERSONNES_MORALES, $company->getId());
 
         if ($subscriptionArray === false) {
             throw new \RuntimeException('An error occured');
