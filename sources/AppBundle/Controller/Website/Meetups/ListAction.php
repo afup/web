f59344e33652a49e11e9819046fec805de68ae25
Nathan Boiron <github@mopolo.net>
2025-06-21 11:21:59 +0200

    Refactor des controllers pour harmoniser

--- sources/AppBundle/Controller/Website/Meetups/ListAction.php
+++ sources/AppBundle/Controller/Website/Meetups/ListAction.php
@@ -2,22 +2,25 @@
 
 declare(strict_types=1);
 
-namespace AppBundle\Controller\Website;
+namespace AppBundle\Controller\Website\Meetups;
 
 use AppBundle\Twig\ViewRenderer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class MeetupsController extends AbstractController
+final class ListAction extends AbstractController
 {
     public function __construct(
         private readonly ViewRenderer $view,
+        #[Autowire('%algolia_app_id%')]
         private readonly string $algoliaAppId,
+        #[Autowire('%algolia_frontend_api_key%')]
         private readonly string $algoliaFrontendApikey,
     ) {}
 
-    public function list(Request $request): Response
+    public function __invoke(Request $request): Response
     {
         return $this->view->render('site/meetups/list.html.twig',[
             'algolia_app_id' => $this->algoliaAppId,
