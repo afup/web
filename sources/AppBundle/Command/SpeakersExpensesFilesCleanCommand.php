c39cba3661b798c70ce5285127f8160a95c0cb1e
Albin Kester <83301974+stakovicz@users.noreply.github.com>
2025-06-24 08:51:59 +0200

    Mise à jour Symfony 7.3 (#1846)
    * Mise à jour Symfony 7.3
    
    * Corrections des tests sur les dates
    
    * Rector

--- sources/AppBundle/Command/SpeakersExpensesFilesCleanCommand.php
+++ sources/AppBundle/Command/SpeakersExpensesFilesCleanCommand.php
@@ -7,21 +7,17 @@ namespace AppBundle\Command;
 use AppBundle\SpeakerInfos\SpeakersExpensesStorage;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(name: 'speaker:expenses-files-clean')]
-class SpeakersExpensesFilesCleanCommand extends Command
+readonly class SpeakersExpensesFilesCleanCommand
 {
-    public function __construct(private readonly SpeakersExpensesStorage $speakersExpensesStorage)
-    {
-        parent::__construct();
-    }
+    public function __construct(private SpeakersExpensesStorage $speakersExpensesStorage) {}
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
+    public function __invoke(OutputInterface $output): int
     {
         $this->speakersExpensesStorage->cleanFiles();
 
-        return self::SUCCESS;
+        return Command::SUCCESS;
     }
 }
