12d4c5ba857f47397296a9b389eaea46d100f16b
vgreb <vgreb@users.noreply.github.com>
2025-08-01 20:43:30 +0200

    refacto: migration de l'administrations des feuilles sous Symfony

--- sources/AppBundle/Site/Form/SheetType.php
+++ sources/AppBundle/Site/Form/SheetType.php
@@ -4,51 +4,48 @@ declare(strict_types=1);
 
 namespace AppBundle\Site\Form;
 
-use AppBundle\Association\Model\Repository\UserRepository;
-use AppBundle\Site\Model\Repository\RubriqueRepository;
 use AppBundle\Site\Model\Repository\SheetRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
-class RubriqueType extends AbstractType
+class SheetType extends AbstractType
 {
     public const POSITIONS_RUBRIQUES = 9;
 
     public function __construct(
-        private readonly RubriqueRepository $rubriqueRepository,
-        private readonly UserRepository $userRepository,
         private readonly SheetRepository $sheetRepository,
     ) {}
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $users = [];
-        foreach ($this->userRepository->getAll() as $user) {
-            $users[$user->getLastName() . ' ' . $user->getFirstName()] = $user->getId();
-        }
         $sheets = [];
         foreach ($this->sheetRepository->getAll() as $sheet) {
             $sheets[$sheet->getName()] = $sheet->getId();
         }
         ksort($sheets, SORT_NATURAL);
+
         $positions = [];
         for ($i = self::POSITIONS_RUBRIQUES ; $i >= -(self::POSITIONS_RUBRIQUES); $i--) {
             $positions[$i] = $i;
         }
-        $rubriques = [];
-        foreach ($this->rubriqueRepository->getAll() as $rubrique) {
-            $rubriques[$rubrique->getNom()] = $rubrique->getId();
-        }
+
         $builder
-            ->add('nom', TextType::class, [
-                'label' => 'Nom de la rubrique',
+            ->add('idParent', ChoiceType::class, [
+                'label' => 'Parent',
+                'choices' => $sheets,
+                'required' => false,
+                'constraints' => [
+                    new Assert\Type("integer"),
+                ],
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
                 'required' => true,
                 'attr' => [
                     'maxlength' => 255,
@@ -60,72 +57,51 @@ class RubriqueType extends AbstractType
                     new Assert\Type('string'),
                 ],
             ])
-            ->add('descriptif', TextareaType::class, [
-                'label' => 'Descriptif',
+            ->add('link', TextType::class, [
+                'required' => true,
+                'label' => 'Lien',
+                'attr' => [
+                    'maxlength' => 255,
+                    'size' => 60,
+                ],
+                'constraints' => [
+                    new Assert\Length(['max' => 255]),
+                    new Assert\NotBlank(),
+                    new Assert\Type('string'),
+                ],
+            ])
+            ->add('alt', TextType::class, [
+                'label' => 'Description',
                 'required' => false,
                 'attr' => [
                     'maxlength' => 255,
-                    'cols' => 42,
-                    'rows' => 10,
-                    'class' => 'tinymce',
+                    'size' => 60,
                 ],
                 'constraints' => [
                     new Assert\Length(['max' => 255]),
                     new Assert\Type('string'),
                 ],
             ])
-            ->add('contenu', TextareaType::class, [
-                'label' => 'Contenu',
-                'required' => true,
-                'attr' => [
-                    'cols' => 42,
-                    'rows' => 20,
-                    'class' => 'tinymce',
-                ],
-                'constraints' => [
-                    new Assert\NotBlank(),
-                    new Assert\Type('string'),
-                    new Assert\NotBlank(),
-                ],
-            ])
-            ->add('icone', FileType::class, [
-                'label' => 'Icône (Taille requise : 43 x 37 pixels)',
+            ->add('image', FileType::class, [
+                'label' => 'Image',
                 'required' => false,
                 'data_class' => null,
                 'constraints' => [
                     new Assert\Image([
-                        'minHeight' => 37,
-                        'maxHeight' => 43,
+                        'mimeTypes' => ['jpg','jpeg','gif','png'],
                     ]),
                 ],
             ])
-            ->add('raccourci', TextType::class, [
-                'required' => true,
-                'label' => 'Raccourci',
+            ->add('imageAlt', TextType::class, [
+                'required' => false,
+                'label' => 'Texte alternatif de l\'image',
                 'attr' => [
                     'maxlength' => 255,
                     'size' => 60,
                 ],
                 'constraints' => [
                     new Assert\Length(['max' => 255]),
-                    new Assert\NotBlank(),
                     new Assert\Type('string'),
-                ],
-            ])
-            ->add('idParent', ChoiceType::class, [
-                'label' => 'Parent',
-                'choices' => $rubriques,
-                'required' => false,
-                'constraints' => [
-                    new Assert\Type("integer"),
-                ],
-            ])
-            ->add('idPersonnePhysique', ChoiceType::class, [
-                'required' => false,
-                'label' => 'Auteur',
-                'choices' => $users,
-                'constraints' => [
-                    new Assert\Type("integer"),
                 ],
             ])
             ->add('date', DateType::class, [
@@ -140,6 +116,7 @@ class RubriqueType extends AbstractType
                     new Assert\Type("datetime"),
                 ],
             ])
+
             ->add('position', ChoiceType::class, [
                 'required' => false,
                 'label' => 'Position ',
@@ -148,13 +125,7 @@ class RubriqueType extends AbstractType
                     new Assert\Type("integer"),
                 ],
             ])
-            ->add('pagination', IntegerType::class, [
-                'required' => false,
-                'constraints' => [
-                    new Assert\Type("integer"),
-                ],
-            ])
-            ->add('etat', ChoiceType::class, [
+            ->add('state', ChoiceType::class, [
                 'label' => 'Etat',
                 'required' => false,
                 'choices' => [
@@ -166,12 +137,11 @@ class RubriqueType extends AbstractType
                     new Assert\Type("integer"),
                 ],
             ])
-            ->add('feuilleAssociee', ChoiceType::class, [
-                'label' => 'Feuille associée',
+            ->add('patterns', TextareaType::class, [
+                'label' => 'Patterns URL',
                 'required' => false,
-                'choices' => $sheets,
                 'constraints' => [
-                    new Assert\Type("integer"),
+                    new Assert\Type('string'),
                 ],
             ])
         ;
