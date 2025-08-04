27b0c77558f230830bf19fb96ef152ceb0a0a85b
Nathan Boiron <github@mopolo.net>
2025-08-04 22:58:51 +0200

    Ajout d'une règle PHPStan pour les fonctions de debug
    Cela permet de détecter les oublis dans les PRs.

---
+++ tests/stubs/phpstan/debug-functions.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+var_dump('a');
+
+print_r('b');
+
+dump('c');
+
+dd('d');
---
+++ tests/unit/AppBundle/StaticAnalysis/Rule/NoDebugFunctionsRuleTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppBundle\Tests\StaticAnalysis\Rule;
+
+use AppBundle\StaticAnalysis\Rule\NoDebugFunctionsRule;
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+
+final class NoDebugFunctionsRuleTest extends RuleTestCase
+{
+    protected function getRule(): Rule
+    {
+        return new NoDebugFunctionsRule();
+    }
+
+    public function testRule(): void
+    {
+        $this->analyse([__DIR__ . '/../../../../stubs/phpstan/debug-functions.php'], [
+            ['Usage of var_dump() is forbidden.', 5],
+            ['Usage of print_r() is forbidden.', 7],
+            ['Usage of dump() is forbidden.', 9],
+            ['Usage of dd() is forbidden.', 11],
+        ]);
+    }
+}
