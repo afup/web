0d04668c1dde997776a456eb63b92d028ceefb30
vgreb <vgreb@users.noreply.github.com>
2025-08-05 19:55:37 +0200

    migration des pages de configuration des catégories bancaires sous Symfony

---
+++ sources/AppBundle/Accounting/Model/Category.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppBundle\Accounting\Model;
+
+use CCMBenchmark\Ting\Entity\NotifyProperty;
+use CCMBenchmark\Ting\Entity\NotifyPropertyInterface;
+
+class Category implements NotifyPropertyInterface
+{
+    use NotifyProperty;
+
+    private ?int $id = null;
+
+    private ?string $name = null;
+
+    private ?int $eventId = null;
+
+    private ?\DateTime $hideInAccountingJournalAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->propertyChanged('id', $this->id, $id);
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getEventId(): ?int
+    {
+        return $this->eventId;
+    }
+
+    public function setEventId(?int $eventId): void
+    {
+        $this->propertyChanged('eventId', $this->eventId, $eventId);
+        $this->eventId = $eventId;
+    }
+
+
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->propertyChanged('name', $this->name, $name);
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getHideInAccountingJournalAt(): ?\DateTime
+    {
+        return $this->hideInAccountingJournalAt;
+    }
+
+    public function setHideInAccountingJournalAt(?\DateTime $hideInAccountingJournalAt): void
+    {
+        $this->propertyChanged('hideInAccountingJournalAt', $this->hideInAccountingJournalAt, $hideInAccountingJournalAt);
+        $this->hideInAccountingJournalAt = $hideInAccountingJournalAt;
+    }
+}
