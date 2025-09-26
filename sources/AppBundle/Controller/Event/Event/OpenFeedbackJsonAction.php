26e06051f08fa2783d95db292b51e7eeb06fc4ac
Stakovicz <albin@les-tilleuls.coop>
2025-09-26 23:35:54 +0200

    Suppression des CORS pour openfeedback

--- sources/AppBundle/Controller/Event/Event/OpenFeedbackJsonAction.php
+++ sources/AppBundle/Controller/Event/Event/OpenFeedbackJsonAction.php
@@ -7,6 +7,7 @@ namespace AppBundle\Controller\Event\Event;
 use AppBundle\Controller\Event\EventActionHelper;
 use AppBundle\Openfeedback\OpenfeedbackJsonGenerator;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 final readonly class OpenFeedbackJsonAction
 {
@@ -15,13 +16,15 @@ final readonly class OpenFeedbackJsonAction
         private OpenfeedbackJsonGenerator $openfeedbackJsonGenerator,
     ) {}
 
-    public function __invoke(string $eventSlug): JsonResponse
+    public function __invoke(string $eventSlug, Request $request): JsonResponse
     {
         $event = $this->eventActionHelper->getEvent($eventSlug);
 
         $response =  new JsonResponse($this->openfeedbackJsonGenerator->generate($event));
 
-        $response->headers->set('Access-Control-Allow-Origin', 'https://openfeedback.io');
+        $response->headers->set('Access-Control-Allow-Origin', $request->server->get('HTTP_ORIGIN'));
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        $response->headers->set('Access-Control-Max-Age', '86400');
 
         return $response;
     }
