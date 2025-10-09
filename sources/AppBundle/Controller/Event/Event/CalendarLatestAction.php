4bf44d43937eea4f3f7d2e942520aee83e7df9ea
Adrien Gallou <adriengallou@gmail.com>
2025-10-09 13:03:35 +0200

    changement de la redirection de m.afup.org

--- sources/AppBundle/Controller/Event/Event/CalendarLatestAction.php
+++ sources/AppBundle/Controller/Event/Event/CalendarLatestAction.php
@@ -18,6 +18,10 @@ final class CalendarLatestAction extends AbstractController
     {
         $event = $this->eventRepository->getCurrentEvent();
 
+        // le calendrier n'est pas utilisable sur mobile, on redirige vers celui d'event.
+        // on ne peux pas le faire de façon automatique, c'est le Forum et je commite depuis la bagagerie,
+        // on fait un hotfix moche mais fonctionnel et verra plus tard comment on gère la correspondance
+        return new RedirectResponse("https://event.afup.org/forum-php-2025/planning/");
         return new RedirectResponse($this->generateUrl('event_calendar', ['eventSlug' => $event->getPath()]));
     }
 }
