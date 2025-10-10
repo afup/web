511cd09a71b94293807eb13efa75cef08aea0333
Adrien Gallou <adriengallou@gmail.com>
2025-10-11 00:14:19 +0200

    adaptations sur synchro meetup suite dépréciation de l'API GraphQL que l'on utilisait
    Le point d'entrée `gql` ne fonctionnait plus.
    
    Il renvoyait toujours une 503, avec pour message :
    ```
    {
          "message": "This API endpoint is deprecated and will be turned off soon. You need to either update your app or migrate to the new API. For migration instructions, please visit: https://www.meetup.com/api/guide/#p06-migration-guide"
    }
    ```
    
    Le guide est effectivement disponible ici : https://www.meetup.com/api/guide/#p06-migration-guide
    
    Parmis les changements constatés sur ce que l'on utilise :
    * Les `upcomingEvents` et `pastEvents` n'existent plus, il faut utiliser un `events` avec des filters pour avoir le même mécanisme
     * on crée des alias graphql pour conserver ces notions et applique bien le filtre en adaptant l'entreée (il n'y a plus d'objet input)
    * Les venues peuvent être multiples (notamment pour les événements en ligne et en présentiel)
     * On adapte les objets utilisés pour récupérer un tableau et en récupère sa première entrée
    * Les dates voient leur précision augmentée
     * On ajoute la seconde dans le `supportDateFormats`

--- tests/unit/AppBundle/Indexation/Meetups/MeetupClientTest.php
+++ tests/unit/AppBundle/Indexation/Meetups/MeetupClientTest.php
@@ -33,12 +33,12 @@ final class MeetupClientTest extends TestCase
     {
         yield [
             'response' => new MockResponse('', ['http_code' => 500]),
-            'expectedExceptionMessage' => 'HTTP 500 returned for "http://fakemeetup/gql".',
+            'expectedExceptionMessage' => 'HTTP 500 returned for "http://fakemeetup/gql-ext".',
         ];
 
         yield [
             'response' => new MockResponse('invalid json'),
-            'expectedExceptionMessage' => 'Syntax error for "http://fakemeetup/gql".',
+            'expectedExceptionMessage' => 'Syntax error for "http://fakemeetup/gql-ext".',
         ];
     }
 
@@ -56,9 +56,11 @@ final class MeetupClientTest extends TestCase
                                             'id' => "12",
                                             'title' => 'Upcoming 1',
                                             'description' => 'Desc 1',
-                                            'dateTime' => '2025-02-11T18:30+01:00',
-                                            'venue' => [
-                                                'name' => 'Lieu 1',
+                                            'dateTime' => '2025-02-11T18:30:00+01:00',
+                                            'venues' => [
+                                                [
+                                                    'name' => 'Lieu 1',
+                                                ]
                                             ],
                                         ],
                                     ],
@@ -67,8 +69,9 @@ final class MeetupClientTest extends TestCase
                                             'id' => "34",
                                             'title' => 'Upcoming 2',
                                             'description' => 'Desc 2',
-                                            'dateTime' => '2025-03-20T18:30+01:00',
-                                            'venue' => null,
+                                            'dateTime' => '2025-03-20T18:30:00+01:00',
+                                            'venues' => [
+                                            ],
                                         ],
                                     ],
                                 ],
@@ -80,8 +83,9 @@ final class MeetupClientTest extends TestCase
                                             'id' => "56",
                                             'title' => 'Past 1',
                                             'description' => 'Desc 3',
-                                            'dateTime' => '2019-04-08T18:30+01:00',
-                                            'venue' => null,
+                                            'dateTime' => '2019-04-08T18:30:00+01:00',
+                                            'venues' => [
+                                            ],
                                         ],
                                     ],
                                     [
@@ -89,9 +93,11 @@ final class MeetupClientTest extends TestCase
                                             'id' => "78",
                                             'title' => 'Past 2',
                                             'description' => 'Desc 4',
-                                            'dateTime' => '2020-10-17T18:30+01:00',
-                                            'venue' => [
-                                                'name' => 'Lieu 2',
+                                            'dateTime' => '2020-10-17T18:30:00+01:00',
+                                            'venues' => [
+                                                [
+                                                    'name' => 'Lieu 2',
+                                                ]
                                             ],
                                         ],
                                     ],
@@ -112,28 +118,28 @@ final class MeetupClientTest extends TestCase
         self::assertEquals(12, $antennes[0]->getId());
         self::assertEquals('Upcoming 1', $antennes[0]->getTitle());
         self::assertEquals('Desc 1', $antennes[0]->getDescription());
-        self::assertEquals(new \DateTime('2025-02-11T18:30+01:00'), $antennes[0]->getDate());
+        self::assertEquals(new \DateTime('2025-02-11T18:30:00+01:00'), $antennes[0]->getDate());
         self::assertEquals('lyon', $antennes[0]->getAntenneName());
         self::assertEquals('Lieu 1', $antennes[0]->getLocation());
 
         self::assertEquals(34, $antennes[1]->getId());
         self::assertEquals('Upcoming 2', $antennes[1]->getTitle());
         self::assertEquals('Desc 2', $antennes[1]->getDescription());
-        self::assertEquals(new \DateTime('2025-03-20T18:30+01:00'), $antennes[1]->getDate());
+        self::assertEquals(new \DateTime('2025-03-20T18:30:00+01:00'), $antennes[1]->getDate());
         self::assertEquals('lyon', $antennes[1]->getAntenneName());
         self::assertNull($antennes[1]->getLocation());
 
         self::assertEquals(56, $antennes[2]->getId());
         self::assertEquals('Past 1', $antennes[2]->getTitle());
         self::assertEquals('Desc 3', $antennes[2]->getDescription());
-        self::assertEquals(new \DateTime('2019-04-08T18:30+01:00'), $antennes[2]->getDate());
+        self::assertEquals(new \DateTime('2019-04-08T18:30:00+01:00'), $antennes[2]->getDate());
         self::assertEquals('lyon', $antennes[2]->getAntenneName());
         self::assertNull($antennes[2]->getLocation());
 
         self::assertEquals(78, $antennes[3]->getId());
         self::assertEquals('Past 2', $antennes[3]->getTitle());
         self::assertEquals('Desc 4', $antennes[3]->getDescription());
-        self::assertEquals(new \DateTime('2020-10-17T18:30+01:00'), $antennes[3]->getDate());
+        self::assertEquals(new \DateTime('2020-10-17T18:30:00+01:00'), $antennes[3]->getDate());
         self::assertEquals('lyon', $antennes[3]->getAntenneName());
         self::assertEquals('Lieu 2', $antennes[3]->getLocation());
     }
