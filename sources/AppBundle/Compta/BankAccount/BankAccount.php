6ec18c40915931ae4173aee4c7a63c5862bff514
Nathan Boiron <github@mopolo.net>
2025-11-18 13:51:05 +0100

    Refactor du DTO des comptes bancaires

--- sources/AppBundle/Compta/BankAccount/BankAccount.php
+++ sources/AppBundle/Compta/BankAccount/BankAccount.php
@@ -4,80 +4,15 @@ declare(strict_types=1);
 
 namespace AppBundle\Compta\BankAccount;
 
-class BankAccount
+final readonly class BankAccount
 {
-    /**
-     * @param string $etablissement
-     * @param string $guichet
-     * @param string $compte
-     * @param string $cle
-     * @param string $domicialisation
-     * @param string $bic
-     * @param string $iban
-     */
     public function __construct(
-        private $etablissement,
-        private $guichet,
-        private $compte,
-        private $cle,
-        private $domicialisation,
-        private $bic,
-        private $iban,
+        public string $etablissement,
+        public string $guichet,
+        public string $compte,
+        public string $cle,
+        public string $domicialisation,
+        public string $bic,
+        public string $iban,
     ) {}
-
-    /**
-     * @return string
-     */
-    public function getEtablissement()
-    {
-        return $this->etablissement;
-    }
-
-    /**
-     * @return string
-     */
-    public function getGuichet()
-    {
-        return $this->guichet;
-    }
-
-    /**
-     * @return string
-     */
-    public function getCompte()
-    {
-        return $this->compte;
-    }
-
-    /**
-     * @return string
-     */
-    public function getCle()
-    {
-        return $this->cle;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDomicialisation()
-    {
-        return $this->domicialisation;
-    }
-
-    /**
-     * @return string
-     */
-    public function getBic()
-    {
-        return $this->bic;
-    }
-
-    /**
-     * @return string
-     */
-    public function getIban()
-    {
-        return $this->iban;
-    }
 }
