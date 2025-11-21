d76d8d54ab4f50011ee68b6f483bed865328db84
vgreb <vgreb@users.noreply.github.com>
2025-11-22 00:22:55 +0100

    test behat

--- sources/AppBundle/Controller/Admin/Membership/MemberType.php
+++ sources/AppBundle/Controller/Admin/Membership/MemberType.php
@@ -8,14 +8,12 @@ enum MemberType: int
 {
     case MemberPhysical = 0;
     case MemberCompany = 1;
-    case AllMembers = 2;
 
     public function getLabel(): string
     {
         return match ($this) {
             self::MemberPhysical => 'Personne physique',
             self::MemberCompany => 'Personne morale',
-            self::AllMembers => 'Tout type de membre',
         };
     }
 }
