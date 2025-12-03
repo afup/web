03ba51f3312008ef85e4b46cec931ba87e881e7d
Nathan Boiron <github@mopolo.net>
2025-12-03 19:18:41 +0100

    Suppression de l'ancien calendrier dynamique (#1986)
    Il n'est plus utilisé ni mis en avant et le code n'est plus maintenu.

--- sources/AppBundle/Controller/Event/Event/CalendarAction.php
+++ sources/AppBundle/Controller/Event/Event/CalendarAction.php
@@ -4,20 +4,13 @@ declare(strict_types=1);
 
 namespace AppBundle\Controller\Event\Event;
 
-use AppBundle\Controller\Event\EventActionHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 final class CalendarAction extends AbstractController
 {
-    public function __construct(
-        private readonly EventActionHelper $eventActionHelper,
-    ) {}
-
     public function __invoke(string $eventSlug): Response
     {
-        $event = $this->eventActionHelper->getEvent($eventSlug);
-
-        return $this->render('event/calendar.html.twig', ['event' => $event]);
+        return $this->redirect('https://event.afup.org');
     }
 }
