e53a371bd0cfdb8ea6dfb2d914e325583ce4c9c9
Nathan Boiron <github@mopolo.net>
2025-12-18 14:43:49 +0100

    Mise en place de DotEnv

--- sources/Afup/Corporate/_Site_Base_De_Donnees.php
+++ sources/Afup/Corporate/_Site_Base_De_Donnees.php
@@ -5,21 +5,17 @@ declare(strict_types=1);
 namespace Afup\Site\Corporate;
 
 use Afup\Site\Utils\Base_De_Donnees;
-use Afup\Site\Utils\Configuration;
 
 class _Site_Base_De_Donnees extends Base_De_Donnees
 {
     public function __construct()
     {
-        /**
-         * @var Configuration $conf
-         */
-        $conf = $GLOBALS['AFUP_CONF'];
-        parent::__construct($conf->obtenir('database_host'),
-            $conf->obtenir('database_name'),
-            $conf->obtenir('database_user'),
-            $conf->obtenir('database_password'),
-            $conf->obtenir('database_port'),
+        parent::__construct(
+            $_ENV['DATABASE_HOST'],
+            $_ENV['DATABASE_NAME'],
+            $_ENV['DATABASE_USER'],
+            $_ENV['DATABASE_PASSWORD'],
+            $_ENV['DATABASE_PORT'],
         );
     }
 }
