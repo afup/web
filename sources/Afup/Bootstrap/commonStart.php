55d8c809fda5a4a27adb87294e5ed909f5376d37
Stakovicz <albin@les-tilleuls.coop>
2026-02-02 22:33:51 +0100

    Refonte - Événements > Inscriptions - Nettoyage

--- sources/Afup/Bootstrap/commonStart.php
+++ sources/Afup/Bootstrap/commonStart.php
@@ -65,66 +65,6 @@ define('AFUP_TRANSPORT_MODE_AVION_ECO', 90);
 define('AFUP_TRANSPORT_MODE_AVION_BUSINESS', 100);
 define('AFUP_TRANSPORT_MODE_COMMUN', 110);
 
-$AFUP_Tarifs_Forum = [
-    Ticket::TYPE_INVITATION => 0,
-    Ticket::TYPE_ORGANIZATION => 0,
-    Ticket::TYPE_SPONSOR => 0,
-    Ticket::TYPE_PRESS => 0,
-    Ticket::TYPE_SPEAKER => 0,
-    Ticket::TYPE_PROJECT => 0,
-    Ticket::TYPE_TEACHER => 0,
-    Ticket::TYPE_DAY_1 => 150,
-    Ticket::TYPE_DAY_2 => 150,
-    Ticket::TYPE_2_DAYS => 250,
-    Ticket::TYPE_2_DAYS_AFUP => 150,
-    Ticket::TYPE_DAY_1_AFUP => 100,
-    Ticket::TYPE_DAY_2_AFUP => 100,
-    Ticket::TYPE_2_DAYS_STUDENT => 150,
-    Ticket::TYPE_DAY_1_STUDENT => 100,
-    Ticket::TYPE_DAY_2_STUDENT => 100,
-    Ticket::TYPE_2_DAYS_EARLY => 150,
-    Ticket::TYPE_2_DAYS_AFUP_EARLY => 150,
-    Ticket::TYPE_EARLY_PLUS_MEMBERSHIP => 150,
-    Ticket::TYPE_DAY_1_STUDENT_EARLY => 100,
-    Ticket::TYPE_DAY_2_STUDENT_EARLY => 100,
-    Ticket::TYPE_2_DAYS_STUDENT_EARLY => 150,
-    Ticket::TYPE_2_DAYS_VOUCHER => 200,
-    Ticket::TYPE_2_DAYS_SPONSOR => 200,
-    Ticket::TYPE_SPECIAL_PRICE => 0,
-];
-
-$GLOBALS['AFUP_Tarifs_Forum'] = $AFUP_Tarifs_Forum;
-
-$AFUP_Tarifs_Forum_Lib = [
-    Ticket::TYPE_INVITATION => 'Invitation',
-    Ticket::TYPE_ORGANIZATION => 'Organisation',
-    Ticket::TYPE_PROJECT => 'Projet PHP',
-    Ticket::TYPE_SPONSOR => 'Sponsor',
-    Ticket::TYPE_PRESS => 'Presse',
-    Ticket::TYPE_TEACHER => 'Enseignement supérieur',
-    Ticket::TYPE_SPEAKER => 'Conferencier',
-    Ticket::TYPE_DAY_1 => 'Jour 1 ',
-    Ticket::TYPE_DAY_2 => 'Jour 2',
-    Ticket::TYPE_2_DAYS => '2 Jours',
-    Ticket::TYPE_2_DAYS_AFUP => '2 Jours AFUP',
-    Ticket::TYPE_DAY_1_AFUP => 'Jour 1 AFUP',
-    Ticket::TYPE_DAY_2_AFUP => 'Jour 2 AFUP',
-    Ticket::TYPE_2_DAYS_STUDENT => '2 Jours Etudiant',
-    Ticket::TYPE_DAY_1_STUDENT => 'Jour 1 Etudiant',
-    Ticket::TYPE_DAY_2_STUDENT => 'Jour 2 Etudiant',
-    Ticket::TYPE_2_DAYS_EARLY => '2 Jours prévente',
-    Ticket::TYPE_2_DAYS_AFUP_EARLY => '2 Jours AFUP prévente',
-    Ticket::TYPE_EARLY_PLUS_MEMBERSHIP => '2 Jours prévente + adhésion',
-    Ticket::TYPE_2_DAYS_STUDENT_EARLY => '2 Jours Etudiant prévente',
-    Ticket::TYPE_2_DAYS_VOUCHER => '2 Jours avec coupon de réduction',
-    Ticket::TYPE_2_DAYS_SPONSOR => '2 Jours par Sponsor',
-    Ticket::TYPE_DAY_1_STUDENT_EARLY => '',
-    Ticket::TYPE_DAY_2_STUDENT_EARLY => '',
-    Ticket::TYPE_SPECIAL_PRICE => 'Tarif Spécial',
-];
-
-$GLOBALS['AFUP_Tarifs_Forum_Lib'] = $AFUP_Tarifs_Forum_Lib;
-
 
 // Initialisation de ting
 $services = new Services();
