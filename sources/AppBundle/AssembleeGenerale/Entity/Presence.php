980d40787f40cf7f663a2487c51a60af814ebf08
vgreb <55197920+vgreb@users.noreply.github.com>
2026-06-23 21:29:34 +0200

    Assemblée Générale - Déclaration des entités Doctrine (#2294)
    Co-authored-by: vgreb <vgreb@users.noreply.github.com>

---
+++ sources/AppBundle/AssembleeGenerale/Entity/Presence.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppBundle\AssembleeGenerale\Entity;
+
+use AppBundle\AssembleeGenerale\Enum\PresenceEtat;
+use AppBundle\Association\Entity\Utilisateur;
+use AppBundle\Doctrine\Type\UnixTimestampType;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'afup_presences_assemblee_generale')]
+class Presence
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    public ?int $id = null;
+
+    #[ORM\Column(type: UnixTimestampType::NAME, nullable: false)]
+    public \DateTime $date;
+
+    #[ORM\ManyToOne(targetEntity: Utilisateur::class)]
+    #[ORM\JoinColumn(name: 'id_personne_physique', referencedColumnName: 'id', nullable: true)]
+    public ?Utilisateur $utilisateur = null;
+
+    #[ORM\Column(nullable: false, enumType: PresenceEtat::class)]
+    public PresenceEtat $presence = PresenceEtat::EnAttente;
+}
