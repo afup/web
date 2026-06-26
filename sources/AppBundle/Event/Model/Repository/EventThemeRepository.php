4da5cbcf50f30092dbe68226ef95e1ca95a2d0bd
vgreb <vgreb@users.noreply.github.com>
2026-06-26 21:29:22 +0200

    Thématisation de la page programme

---
+++ sources/AppBundle/Event/Model/Repository/EventThemeRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppBundle\Event\Model\Repository;
+
+use AppBundle\Event\Model\EventTheme;
+use CCMBenchmark\Ting\Repository\CollectionInterface;
+use CCMBenchmark\Ting\Repository\HydratorSingleObject;
+use CCMBenchmark\Ting\Repository\Repository;
+
+/**
+ * @extends Repository<EventTheme>
+ */
+class EventThemeRepository extends Repository
+{
+    /**
+     * @return CollectionInterface<EventTheme>
+     */
+    public function getByEventOrderedByPriority(int $eventId): CollectionInterface
+    {
+        return $this->getPreparedQuery(
+            'SELECT * FROM afup_conference_theme WHERE id_forum = :idForum ORDER BY priority ASC, name ASC',
+        )->setParams(['idForum' => $eventId])->query($this->getCollection(new HydratorSingleObject()));
+    }
+}
