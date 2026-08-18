c65b3d08a7c0816ac78f5e0154e742e2f0cbd48a
Nathan Boiron <github@mopolo.net>
2026-08-18 23:19:59 +0200

    Récupération des données des interviews
    Quand on arrive sur la page de liste des interviews, s'il n'y a pas d'id
    d'event en session, on va récupérer le plus récent.
    
    Dans ce cas, l'event est chargé sans les données wordpress pour les
    interviews, et la page de gestion des interviews affiche ces données
    comme manquantes, ce qui force une navigation inutile.

--- sources/AppBundle/Event/Model/Repository/EventRepository.php
+++ sources/AppBundle/Event/Model/Repository/EventRepository.php
@@ -44,7 +44,7 @@ class EventRepository extends Repository implements MetadataInitializer
     public function getNextEvents(): ?CollectionInterface
     {
         $query = $this
-            ->getQuery('SELECT id, path, titre, text, date_debut, date_fin, date_debut_appel_conferencier, date_fin_appel_conferencier, date_fin_vente, nb_places FROM afup_forum WHERE date_debut > NOW() ORDER BY date_debut')
+            ->getQuery('SELECT id, path, titre, text, date_debut, date_fin, date_debut_appel_conferencier, date_fin_appel_conferencier, date_fin_vente, nb_places, interviews_wp_category_id, interviews_intro, interviews_cta_text FROM afup_forum WHERE date_debut > NOW() ORDER BY date_debut')
         ;
 
         $events = $query->query($this->getCollection(new HydratorSingleObject()));
